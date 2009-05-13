a384fb7f7a5005d898fce5448c2929019276bb21
PTruskey <PTruskey@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2009-05-13 19:53:40 +0000

    added mean and covariance matrix computation to the javascript!

--- testweb/data.pl
+++ testweb/data.pl
@@ -1,10 +1,27 @@
 #! /usr/bin/perl
 print "Content-type: text/xml\n\n"; # mime-type! 
-
-read(STDIN, $request,$ENV{'CONTENT_LENGTH'}) 
-		|| die "Could not get query\n";
-if($request =~ /samples=(\d+)/ ){
-	print "<p>you requested $1 samples.</p>\n"; 
+my $request ; 
+# open data file -- in real life this would be from SDRAM on myopen. 
+my $source = "data.txt"; # must be in same dir.. 
+open(FH, "< $source"); 
+local( $/ ); # this allows us to slurp the whole thing.. 
+my $data = <FH>; 
+close FH; 
+my $range = 5000;
+my $random_number = int(rand($range));
+#@array = ("3499", "234", "1456", "345" ,"2246", "1235", "3215", "9482", "351" );
+@array = ("$random_number");
+@feats = (1, 1, 1, 1, 1, 1);
+read(STDIN, $request,$ENV{'CONTENT_LENGTH'}) ; 
+if( not defined ($ENV) ){
+	$request = "samples=$range"; 
 }
-print qq(<font color="purple"><p>some simulated data here!</p>\n); 
-print qq(<p>450,345,556,100</p></font>\n); 
+if($request =~ /samples=(\d+)/ ){
+	#first line should be the matrix size. 
+	print "<div>rows:5000 cols:4\n"; 
+	#print "you requested $1 samples\n"; 
+}
+print qq($data\n</div>); 
+#print qq(<p>@array</p></font>\n); 
+#print qq(<p> The features are: @feats</p>);
+
--- testweb/gmm_test.js
+++ testweb/gmm_test.js
@@ -1,47 +1,6 @@
-/*
-var nfeat = feats.length
 
-var n = 4
-var m = nfeat
 
-// indexes the data into a matrix
 
-data2: function(data) {
-var i elements = data.elements || data;
-if (typeof(elements[0][0])!= 'undefined'){
-  var ni = elements.length, ki = ni, nj, kj, j;
-  this.elements = [];
-do { i = ki - ni;
- nj = elements[i].length; kj = nj;
-        this.elements[i] = [];
-        do { j = kj - nj;
-          this.elements[i][j] = elements[i][j];
-        } while (--nj);
-      } while(--ni);
-      return this;
-    }		// need to resize matrix so it is (1024*5) x 9 
-    var n = elements.length, k = n; // need 4x4 of these matricies
-    this.elements = [];
-    do { i = k - n;
-      this.elements.push([elements[i]]);
-    } while (--n);
-    return this;
-  }
-};
-
-// creates a zeros matrix 
-f.zerochannel: function (n, m) {
-  var els = [], ni = n, i, nj, j;
-  do { i = n - ni;
-    els[i] = [];
-    nj = m;
-    do { j = m - nj;
-      els[i][j] = 0;
-    } while (--nj);
-  } while (--ni);
-  return Matrix.create(els);
-}; // have (78*4)*9 of these matricies
-*/
 function retrieveData(samples)
 {       //id is the pid number in the sql database. 
         //div is the division id to replace in the xml schema.
@@ -68,11 +27,11 @@ function retrieveUpdate(){
 			//result is the data from myopen board!!
 			//need to process it here (call math functions)
 			// and return result. 
-			result = result + "some result via JS" ; 
+			processed = processData(result); 
 			var node = document.getElementById("-2"); 
 			//var par = node.parentNode;
 			//var rep = node.cloneNode(true); 
-			node.innerHTML = result;
+			node.innerHTML = processed;
 			//rep.setAttribute("id", "-1"); //this one to be replaced!
 			//par.insertBefore(rep, node);
                 } else {
@@ -80,3 +39,156 @@ function retrieveUpdate(){
                 }
         }
 }
+
+function processData(d){
+	var res = " "; //what is to be returned.
+	var mat = d.split("\n"); 
+	//first line is the matrix size. 
+	var header = mat[0] ; 
+	//alert( "header=" + header); 
+	var matches = header.match( /\b\d+\b/g );
+	//var matches = re.exec(header); 
+	var rows = parseInt(matches[0]); 
+	var cols = parseInt(matches[1]); 
+	alert( "rows=" + rows + " cols=" + cols); 
+	var m = Matrix.Zero(rows, cols); 
+	var me = m.elements ;
+	for( var r = 0; r<rows; r++ ){
+		var row = mat[r + 1]; 
+		var col = row.split(" ");  
+		for( c = 0; c<cols; c++ ){
+			var samp = col[c] ; 
+			me[r][c] = parseFloat(samp); 
+		}
+	}
+	var mean = calcMean(m); 
+	var cov = calcCov(m, mean); 
+	res = res + printMatrix(cov); 
+	res = res + printVector(mean); 
+	
+	return res ; 
+}
+
+function calcCov(m,mean){
+	var mz = subMean(m,mean); 
+	//now compute the covariance matrix. 
+	var n = mz.transpose(); 
+	var cov = n.x(mz) ;
+	var cove = cov.elements ; 
+	//divide by the number of elements...
+	for (i =0; i<cov.rows(); i++){
+		for (j=0; j < cov.cols(); j++){
+			cove[i][j] = cove[i][j] / m.rows() ; 
+		}
+	}
+	return cov ; 
+}
+
+function calcMean(m){
+	var me = m.elements ; 
+	var sum = Vector.Zero(m.cols());
+	var sume = sum.elements;
+	for (i =0; i<m.rows(); i++){
+		for (j=0; j < m.cols(); j++){
+			sume[j] = sume[j] + me[i][j];
+		}
+	}
+	for (j=0; j < m.cols(); j++){
+		sume[j] = sume[j] / m.rows();
+	}
+	return sum;
+}
+
+function subMean(m, mean) {
+	//subtract the mean from a matrix. 
+	//assumes that the mean is taken along the rows.
+	//not sure if javascript uses references .. ?
+	var n = Matrix.Zero(m.rows(), m.cols()); 
+	var me = m.elements ; 
+	var ne = n.elements ; 
+	var se = mean.elements; 
+	for (i =0; i<m.rows(); i++){
+		for (j=0; j < m.cols(); j++){
+			ne[i][j] = me[i][j] - se[j] ; 
+		}
+	}
+	return n ; 
+}
+
+function printMatrix (p) {	
+	var pe = p.elements ; 
+	var res = "<table><font color=\"green\">\n"; 
+	for ( i = 0; i<p.rows(); i++){
+		res = res + "<tr>\n"; 
+		for( j=0; j<p.cols(); j++){
+			res = res + "<td>" + pe[i][j] + "</td>\n"; 
+		}
+		res = res + "</tr>\n"; 
+	}
+	res = res + "</font></table>\n"; 
+	return res ; 
+}
+
+function printVector (p) {	
+	var pe = p.elements ; 
+	var res = "<table><font color=\"green\">\n"; 
+	for ( i = 0; i<p.dimensions(); i++){
+		res = res + "<tr>\n"; 
+		res = res + "<td>" + pe[i] + "</td>\n"; 
+		res = res + "</tr>\n"; 
+	}
+	res = res + "</font></table>\n"; 
+	return res ; 
+}
+
+/*
+// creates a zeros matrix 
+f.zerochannel: function (n, m) {
+	var els = [], ni = n, i, nj, j;
+		do { i = n - ni;
+		els[i] = [];
+		nj = m;
+		do { j = m - nj;
+		els[i][j] = 0;
+		} 
+	while (--nj);
+	} 
+	while (--ni);
+	return Matrix.create(els);
+}; // have (78*4)*9 of these matricies
+
+var nfeat = feats.length
+
+var n = 4
+var m = nfeat
+
+// indexes the data into a matrix
+
+data2: function(data) {
+var i elements = data.elements || data;
+if (typeof(elements[0][0])!= 'undefined'){
+  var ni = elements.length, ki = ni, nj, kj, j;
+  this.elements = [];
+do { i = ki - ni;
+ nj = elements[i].length; kj = nj;
+        this.elements[i] = [];
+        do { j = kj - nj;
+          this.elements[i][j] = elements[i][j];	
+        } while (--nj);
+      } while(--ni);
+      return this;
+    }		// need to resize matrix so it is (1024*5) x 9 
+    var n = elements.length, k = n; // need 4x4 of these matricies
+    this.elements = [];
+    do { i = k - n;
+      this.elements.push([elements[i]]);
+    } while (--n);
+    return this;
+  }
+};
+
+
+var class 
+for (class = 1; class<= 9; class = class++); {
+	
+*/
