dff9938b9bc791f0548352928b98408b5963dca0
PTruskey <PTruskey@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2009-05-20 20:54:06 +0000

    added matrix size to beginning of data text file.

--- trunk/testweb/gendata.m
+++ trunk/testweb/gendata.m
@@ -1,7 +1,10 @@
 x = data4; 
-f = fopen('data4.txt', 'w'); 
-for j = 1:size(x,1)
-   for k = 1:size(x,2)
+f = fopen('data4.txt', 'w');
+r = size(x,1); 
+c = size(x,2); 
+fprintf(f,['rows:' num2str(r) ' cols:' num2str(c)]); 
+for j = 1:r
+   for k = 1:c
        fwrite(f, [num2str(x(j,k)) ' ']); 
    end
    fprintf(f, '\n'); 
