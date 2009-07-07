51f902a0fa2aa76b803647d11a3a3154da0e06cf
PTruskey <PTruskey@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2009-07-07 13:45:26 +0000

    added fully data file as binary

--- testweb/gendata.m
+++ testweb/gendata.m
@@ -1,7 +1,9 @@
-x = x; 
+x = Data; 
 f = fopen('datalda.txt', 'w');
 r = size(x,1); 
 c = size(x,2); 
+span = (max(x) - min(x));
+scale = (1./span)*10000;
 fprintf(f,['rows:' num2str(r) ' cols:' num2str(c) '\n']); 
 for j = 1:r
    for k = 1:c
@@ -11,10 +13,10 @@ for j = 1:r
 end
 fclose(f); 
 
-f = fopen('databin.bin', 'w');
+f = fopen('datafull.bin', 'w');
 r = size(x,1); 
 c = size(x,2); 
 fwrite(f,r,'integer*4');
 fwrite(f,c,'integer*4');
-fwrite(f,x,'integer*2'); 
+fwrite(f,x*span,'integer*2'); 
 fclose(f); 
