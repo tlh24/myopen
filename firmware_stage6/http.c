32790dbd13be37434f3c733f1b8a2366e58f6559
sideskate <sideskate@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2010-11-03 01:53:50 +0000

    working on getting stage6 / bridge booting.  Processor does not seem to want to boot off of the flash - may decide to make other boards while I wait for the correct serial flash parts.

---
+++ firmware_stage6/http.c
@@ -0,0 +1,371 @@
+// for serving up configuration pages.  
+#include "memory.h"
+#include "util.h"
+#include "lcd.h"
+#include "http.h"
+#include "ethernet.h"
+
+u8 g_enableUDP; 
+u32	g_httpHeaderLen; 
+u32	g_httpContentLen; 
+u8*	g_httpSendPtr; 
+u32	g_httpRemainingLen; 
+u32	g_httpRxed;  //number of bytes received in a multi-packet transfer.
+
+int httpCollate(char* payload, int paylen){
+	if( g_httpRxed ){
+		//then there is already data buffered -- merge it. 
+		u8* p = (u8*)HTTP_RX; 
+		p += g_httpRxed; 
+		memcpy_((u8*)payload, p, paylen); 
+		g_httpRxed += paylen; 
+		//see if the new data is a complete request. 
+		if( httpResp((char*)HTTP_RX, g_httpRxed) ){
+			g_httpRxed = 0; //used the data.
+			return 1;
+		}
+	}else {
+		//see if this individual packet is a request. 
+		//if it is not, copy it over. 
+		if( httpResp(payload, paylen) ){
+			g_httpRxed = 0; //used the data.
+			return 1;
+		} else {
+			//copy the data to the buffer, multiple packets.  
+			memcpy_((u8*)payload, (u8*)HTTP_RX, paylen); 
+			g_httpRxed = paylen; 
+		}
+	}
+	return 0; 
+}
+
+int httpResp( char* payload, int paylen ){
+	if( paylen > 14 && strcmp(payload, "GET / HTTP")){ 
+		//start ( '/' is the trailing part of the URL )
+		if(substr("\r\n\r\n", payload, paylen)){ //end
+			//requested a webpage, in one packet.
+			//printf_str("requested a webpage!\n"); 
+			g_httpContentLen = htmlDefault(); 
+			//content length seems to be minus the headers, the actual content length (surprise!)
+			httpHeader(); 
+			//printf_int("header length:", g_httpHeaderLen); 
+			//printf_str("\n"); 
+			//printf_int("content length:", g_httpContentLen); 
+			//printf_str("\n"); 
+			return 1; 
+		}
+	}else if(paylen >= 18 && substr("POST /data", payload, paylen)){
+		// get the specified past # ms of data. 
+		int pos = substr("rawLen=", payload, paylen); 
+		if(pos){
+			char* p; 
+			p = payload; p+= pos; 
+			int rawlen = atoi(p, 10); 
+			if(rawlen >= 32 && rawlen <= 4000){
+				u32* wr_ptr = pWR_PTR; 
+				u32 ptr = ((*wr_ptr) & 0x3ffe0) - (rawlen*16*2); 
+				u32* dest = (u32*)HTTP_CONTENT; 
+				u32* src = (u32*)ptr; 
+				int i; 
+				for(i=0; i< rawlen*8; i++){
+					src = (u32*) ( ((u32)src) & 0x3ffff ); 
+					*dest++ = *src++;
+				}
+				g_httpContentLen = rawlen * 32; 
+				httpHeader(); 
+			} else {
+				char* dest = (char*)HTTP_CONTENT; 
+				int len = 0; 
+				dest = strcpy_(dest, &len, "/data: rawLen unspecified or out of range"); 
+				g_httpContentLen = len; 
+				httpHeader(); 
+			}
+			return 1; 
+		} 
+	}else if(paylen >= 18 && substr("POST",payload,paylen) ){
+		char paramChanged = 0; 
+		if(substr("data_stream=",payload, paylen) ){
+			if(substr("=Enable",payload, paylen) ){
+				//then turn on the data stream! 
+				u32* wr_ptr = pWR_PTR; 
+				u32* tr_ptr = pTR_PTR; 
+				g_streamEnabled = 1; 
+				(*tr_ptr) = (*wr_ptr);  //so that we don't spend a long time catching up.
+				paramChanged = 1; 
+			}
+			if(substr("=Disable",payload, paylen)){
+				g_streamEnabled = 0; 
+				paramChanged = 1; 
+			}
+		}
+		if(substr("filter_data",payload,paylen)){
+			if(substr("=Enable",payload, paylen) ){
+				*pRAW_ENAB = 0; //enable filter = disable raw.
+				paramChanged = 1; 
+			}
+			if(substr("=Disable",payload, paylen)){
+				*pRAW_ENAB = 1; //enable filter = disable raw.
+				paramChanged = 1; 
+			}
+		}
+		// --- look at the mouse channels. 
+		char* p; 
+		int pos ; 
+		pos = substr("xpos_chan=",payload,paylen); 
+		if(pos){
+			p = payload; p += pos; 
+			g_mouseXpos = (u8)(PhysicalToLogicalChan(atoi( p, 3)) & 0xff); 
+			paramChanged = 1; 
+		}
+		pos = substr("ypos_chan=",payload,paylen); 
+		if(pos){
+			p = payload; p += pos; 
+			g_mouseYpos = (u8)(PhysicalToLogicalChan(atoi( p, 3)) & 0xff); 
+			paramChanged = 1; 
+		}
+		pos = substr("xneg_chan=",payload,paylen); 
+		if(pos){
+			p = payload; p += pos; 
+			g_mouseXneg = (u8)(PhysicalToLogicalChan(atoi( p, 3)) & 0xff); 
+			paramChanged = 1; 
+		}
+		pos = substr("yneg_chan=",payload,paylen); 
+		if(pos){
+			p = payload; p += pos; 
+			g_mouseYneg = (u8)(PhysicalToLogicalChan(atoi( p, 3)) & 0xff); 
+			paramChanged = 1; 
+		}
+		//set the destination IP
+		pos = substr("ip1=",payload,paylen); 
+		if(pos){
+			p = payload; p+= pos; 
+			u32 tip = atoi(p,3) & 0xff; 
+			NetDataDestIP &= 0xffffff00 ; //big-endian order.
+			NetDataDestIP += tip; 
+			paramChanged = 1; 
+		}
+		pos = substr("ip2=",payload,paylen); 
+		if(pos){
+			p = payload; p+= pos; 
+			u32 tip = atoi(p,3) & 0xff; 
+			NetDataDestIP &= 0xffff00ff ; 
+			NetDataDestIP += tip << 8; 
+			paramChanged = 1; 
+		}
+		pos = substr("ip3=",payload,paylen); 
+		if(pos){
+			p = payload; p+= pos; 
+			u32 tip = atoi(p,3) & 0xff; 
+			NetDataDestIP &= 0xff00ffff ; 
+			NetDataDestIP += tip << 16; 
+			paramChanged = 1; 
+		}
+		pos = substr("ip4=",payload,paylen); 
+		if(pos){
+			p = payload; p+= pos; 
+			u32 tip = atoi(p,3) & 0xff; 
+			NetDataDestIP &= 0x00ffffff ; 
+			NetDataDestIP += tip << 24; 
+			paramChanged = 1; 
+		}
+		// --------------------------
+		if(paramChanged){
+			g_httpContentLen = htmlDefault(); 
+			httpHeader(); 
+			return 1; 
+		}
+		// calibrate thresholds! 
+		if(substr("calibrate=Cal",payload,paylen)){
+			//look at the last 2 seconds of data & measure mean. 
+			//*assuming that the absolute value has been taken*
+			int sum[16]; 
+			int i,j; 
+			for(i=0; i<16;i++){ sum[i] = 0; }
+			u32* wr_ptr = pWR_PTR; 
+			u32 ptr = ((*wr_ptr) & 0x3ffe0) - (16*2048*2); 
+			ptr &= 0x3ffff; 
+			u16* p = (u16*)ptr; 
+			for(j=0; j<2048; j++){
+				for(i=0; i<16; i++){
+					sum[i] += *p++; 
+					p = (u16*)((u32)p & 0x3ffff); 
+				}
+			}
+			for(i=0; i<16;i++){
+				sum[i] = sum[i] >> 11; 
+			}
+			int len = htmlDefault(); 
+			char* dest = ((char*)HTTP_CONTENT); 
+			len -= 19;  //overwrite the </body> </html>
+			dest += len;
+			dest = strcpy_(dest, &len, "<table>\n"); 
+			for(i=0; i<16; i++){
+				int logical = PhysicalToLogicalChan(i); 
+				dest = strcpy_(dest, &len, "<tr><td>Channel "); 
+				dest = strprintf_int(dest, &len, i ); 
+				dest = strcpy_(dest, &len, "</td><td>mean "); 
+				dest = strprintf_int(dest, &len, sum[logical]); 
+				dest = strcpy_(dest, &len, "</td></tr>\n"); 
+			}
+			dest = strcpy_( dest, &len, "</table></body></html>\r\n" ); 
+			g_httpContentLen = len ; 
+			httpHeader(); 
+			return 1; 
+		}
+	} else {
+		if(substr("\r\n\r\n", payload, paylen)){ //end .. of some sort of request! 
+			g_httpContentLen = html404(); 
+			httpHeader(); 
+			return 1; 
+		}
+	}
+	return 0; //nope, was not able to do anything with it.
+}
+
+void httpHeader(){
+	char* h = ((char*)HTTP_HEADER); 
+	int txlen = 0; 
+	h = strcpy_(h, &txlen, "HTTP/1.1 200 OK\r\n"); 
+	h = strcpy_(h, &txlen, "Server: Myopen\r\n");
+	h = strcpy_(h, &txlen, "Content-Length: "); 
+	h = strprintf_int(h, &txlen, g_httpContentLen); 
+	h = strcpy_(h, &txlen, "\r\nContent-Type: text/html\r\n\r\n"); 
+	g_httpHeaderLen = txlen; 
+}
+int html404(){
+	char* dest = ((char*)HTTP_CONTENT); 
+	int len = 0; 
+	dest = strcpy_(dest, &len, "HTTP/1.1 404 Not Found\r\n");
+	return len; 
+}
+int htmlGeneric(char* str){
+	char* dest = ((char*)HTTP_CONTENT); 
+	int len = 0; 
+	strcpy_(dest, &len, str); 
+	g_httpContentLen = len; 
+	httpHeader(); 
+	return len;
+}
+int htmlDefault(){
+	char* dest = ((char*)HTTP_CONTENT); 
+	//fills *dest with the default webpage.
+	int len = 0; 
+	int j = 0; 
+	dest = strcpy_(dest, &len, 
+	"<html><head><title>myopen</title></head>\n<body>\n<h1>Myopen configuration</h1>\n"); 
+	//------------------
+	dest = htmlDiv(dest, &len, 'b'); 
+	dest = htmlForm(dest, &len); 
+	dest = strcpy_(dest, &len, 
+		"Data Stream (port 4340, UDP)\n\
+		<input type=\"submit\" name=\"data_stream\" value="); 
+	if(g_streamEnabled){
+		dest = strcpy_(dest, &len, "\"Disable\""); 
+	}else {
+		dest = strcpy_(dest, &len, "\"Enable\""); 
+	}
+	dest = strcpy_(dest, &len, "/>\n</form></div>\n"); 
+	//------------------
+	dest = htmlDiv(dest, &len, 'b'); 
+	dest = htmlForm(dest, &len); 
+	dest = strcpy_(dest, &len, "Data Stream Destination IP\n"); 
+	for( j=1; j<=4; j++){
+		dest = strcpy_(dest, &len, "<input type=\"text\" name=\"ip"); 
+		dest = strprintf_int(dest, &len, j); 
+		dest = strcpy_(dest, &len, "\" size=\"3\"/ value=\""); 
+		dest = strprintf_int(dest, &len, (NetDataDestIP >> (j-1)*8)& 0xff ) ; //big endian format.
+		dest = strcpy_(dest, &len, "\"/>\n"); 
+	}
+	dest = strcpy_(dest, &len, "<input type=\"submit\" value=\"Set\""); 
+	dest = strcpy_(dest, &len, "/>\n</form></div>\n"); 
+	//------------------
+	u8* raw_enab = pRAW_ENAB; 
+	dest = htmlDiv(dest, &len, 'g'); 
+	dest = htmlForm(dest, &len); 
+	dest = strcpy_(dest, &len, 
+		"Filter Data\n\
+		<input type=\"submit\" name=\"filter_data\" value="); 
+	if( (*raw_enab) & 0x1){
+		dest = strcpy_(dest, &len, "\"Enable\""); 
+	}else {
+		dest = strcpy_(dest, &len, "\"Disable\""); 
+	}
+	dest = strcpy_(dest, &len, "/>\n</form></div>\n"); 
+	//------------------
+	dest = htmlDiv(dest, &len, 'b'); 
+	dest = htmlForm(dest, &len); 
+	dest = strcpy_(dest, &len, "Cursor control channels:\n<table>\n"); 
+	dest = htmlCursorSelect(dest, &len, "+X (right)", "xpos_chan", 
+		LogicalToPhysicalChan(g_mouseXpos) ); 
+	dest = htmlCursorSelect(dest, &len, "-X (left)", "xneg_chan", 
+		LogicalToPhysicalChan(g_mouseXneg) ); 
+	dest = htmlCursorSelect(dest, &len, "+Y (down)", "ypos_chan", 
+		LogicalToPhysicalChan(g_mouseYpos) ); 
+	dest = htmlCursorSelect(dest, &len, "-Y (up)", "yneg_chan", 
+		LogicalToPhysicalChan(g_mouseYneg) ); 
+	dest = strcpy_(dest, &len, 
+		"</table>\n<input type=\"submit\" value=\"Save\"/>\n</form></div>\n");
+	
+	dest = htmlDiv(dest, &len, 'g'); 
+	dest = htmlForm(dest, &len); 
+	dest = strcpy_(dest, &len, 
+		"<input type=\"submit\" name=\"calibrate\" value=\"Calibrate Thresholds\"/>\n\
+		</form>\n</div>\n"); 
+		
+	dest = htmlDiv(dest, &len, 'b'); 
+	dest = strcpy_(dest, &len, "<form action=\"/data\" method=\"POST\"/>\n"); 
+	dest = strcpy_(dest, &len, "get last<input type=\"submit\" name=\"rawLen\" value=\"1500\"/> samples<br"); 
+	dest = strcpy_(dest, &len, "/>\n</form></div>\n"); 
+	//printout an 'uptime' indicator. 
+	j = GTIME; 
+	dest = strcpy_(dest, &len, " uptime "); 
+	int s = j/1000;
+	int m = s/60; 
+	dest = strprintf_int(dest, &len, m);
+	dest = strcpy_(dest, &len, ":"); 
+	dest = strprintf_int(dest, &len, s - m*60);
+	dest = strcpy_(dest, &len, "."); 
+	dest = strprintf_int(dest, &len, j - s*1000); 
+	//
+	dest = strcpy_(dest, &len, "\n</body>\n</html>\r\n\r\n"); 
+	return len; 
+}
+char* htmlDiv(char* dest, int* len, char color){
+	if(color == 'b'){
+		dest = strcpy_(dest, len, 
+			"<div style=\"background:#dedeee\" align=\"center\">\n"); 
+	}
+	else {
+		dest = strcpy_(dest, len, 
+			"<div style=\"background:#d8eee0\" align=\"center\">\n"); 
+	}
+	return dest; 
+}
+char* htmlForm(char* dest, int* len){
+	return strcpy_(dest, len, "<form action=\"/\" method=\"POST\">"); 
+}
+char* htmlCursorSelect(char* dest, int* len, char* desc, 
+			char* selectName, int selectedNum){
+	dest = strcpy_(dest, len, "<tr><td>"); 
+	dest = strcpy_(dest, len, desc); 
+	dest = strcpy_(dest, len, "</td>\n<td><select name=\""); 
+	dest = strcpy_(dest, len, selectName); 
+	dest = strcpy_(dest, len, "\">\n"); 
+	int i; 
+	for(i=0; i<16; i++){
+		dest = htmlCursorOption(dest, len, i, selectedNum); 
+	}
+	dest = strcpy_(dest, len, "</select></td></tr>\n"); 
+	return dest; 
+}
+char* htmlCursorOption(char* dest, int* len, int num, int selectedNum){
+	dest = strcpy_(dest, len, "<option value="); 
+	dest = strprintf_int(dest, len, num); 
+	if(num == selectedNum ){
+		dest = strcpy_(dest, len, " selected=\"selected\""); 
+	}
+	dest = strcpy_(dest, len, ">"); 
+	dest = strprintf_int(dest, len, num); 
+	dest = strcpy_(dest, len, "</option>\n"); 
+	return dest; 
+}
---
+++ firmware_stage6/tcp.c
@@ -0,0 +1,313 @@
+#include <cdefBF537.h>
+#include "memory.h"
+#include "util.h"
+#include "lcd.h"
+#include "ethernet.h"
+#include "http.h"
+
+u32 TcpState; 
+u32 TcpSeqClient; //the last SYN/ACK packet recieved. host order.
+u32 TcpSeqHost; 		// our counter. host byte order.
+u16 TcpClientPort; //from which port a client is querying us. 
+					// IN NETWORK ORDER
+u32 TcpSeqHttpStart; //the sequence number of the start of a HTTP PDU. 
+u32 TcpHighestAck ; //the largest ACKed byte that the reciever claims to have gotten. 
+u32 TcpHighestAckTimeout; //when we should go back and transmit from HighestAck
+u32 TcpBurstOffset ; 
+
+u8* tcp_packet_setup(int len, char* result, u32 dest, u8 flags, u32 seq, u32 ack){
+	//setup a tcp packet, caller sends it. 
+	//len is the payload length.
+	
+	int length; 
+	u8* data; 
+	
+	length = sizeof(tcp_packet) + len; 
+	data = eth_header_setup(&length, result, dest);
+	if( *result < 0 ) return 0; 
+	//printf_int("tcp packet setup: ip packet length: ", length); 
+	//printf_str("\n"); 
+	data = ip_header_setup(data, &length, dest, IP_PROT_TCP); 
+	data = tcp_header_setup(data, &length, flags, seq, ack); 
+	
+	return data; 
+}
+
+u16 tcp_checksum_calc(u8* data, int length){
+	// length is the length of tcp payoad data
+	// http://mathforum.org/library/drmath/view/54379.html
+	int i; 
+	tcp_packet* p; 
+	p = (tcp_packet*)data;
+	
+	u32 sum = 0;
+	u16* ptr; 	
+	//first, add up the IP pseudo-header. 
+	u32* pp = &(p->ip.src) ; 
+	ptr = (u16*)( pp ); 
+	for(i=0; i<4; i++){
+		sum += *ptr++; 
+	}
+	sum += ((p->ip.p) << 8) & 0xffff; 
+	sum += htons(length + ((p->tcp.dataoff)>>2)); 
+	
+	if(length & 0x1){ //pad with zero. 
+		u8* c = (u8*)(&(p->tcp));
+		c +=  length + ((p->tcp.dataoff) >> 2); 
+		*c = 0; 
+		length++; 
+	}
+	//checksum the actual packet.
+	p->tcp.xsum = 0; 
+	ptr = (u16*)( &(p->tcp) ); 
+	for(i=0; i < length/2 + ((p->tcp.dataoff)>>3); i++){
+		sum += *ptr++;
+	}
+	//manage the carries
+	sum = (sum & 0xffff) + (sum >> 16);
+	return ~(sum & 0xffff); 
+	//don't think we need to change the byte-order, as we are 
+	//operating on stuff that is network-byte order, ready to transmit.
+}
+
+void tcp_checksum_set(int length){
+	//assumes that we are working on a packet in an outgoing dma buffer.
+	//length is the payload length.
+	tcp_packet* p; 
+	p = (tcp_packet*)(txbuf[txIdx]->FrmData); 
+	p->tcp.xsum = tcp_checksum_calc((u8*)(txbuf[txIdx]->FrmData), length);
+}
+u8 tcp_checksum_check(u8* data, int length){
+	//assumes that we are working on a packet in the RX buffer. 
+	tcp_packet* p; 
+	u16 sump, sumc; 
+	p = (tcp_packet*)data; 
+	sump = p->tcp.xsum ; 
+	sumc = tcp_checksum_calc(data, length); 
+	//note: includes the present checksum; 
+	// set to zero if this is a packet to be transmitted. 
+	if(sump == sumc){
+		//printf_hex("TCP checksum correct, is:", sump); 
+		//printf_str("\n"); 
+		return 1; 
+	}else{
+		printf_hex("TCP checksum incorrect, is:", sump); 
+		printf_hex(" calculated: ", sumc); 
+		printf_str("\n"); 
+		return 0; 
+	}
+}
+
+int tcp_length(tcp_packet* p){
+	//calculate the payload length in bytes & return it. 
+	int length = htons(p->ip.len);
+	length -= (p->ip.hl_v & 0x0f) << 2; // specifies 32 bit words, so only shift 2.
+	length -= (p->tcp.dataoff & 0xf0)  >> 2; //same here, 'cept the other direction. (>>4 , << 2)
+	return length; 
+}
+
+//need to keep track of the bytes that were acknowleged. 
+
+int tcp_rx(u8* data, int length){
+	tcp_packet* p; 
+	int rxlen; 
+	char* payload, result;
+	u8* tx; 
+
+	p = (tcp_packet*)data; 
+	if(p->eth.protLen == htons(ETH_PROTO_IP4)){
+		if(p->ip.p == IP_PROT_TCP && 
+		    p->tcp.dest == htons(80) && 
+			ip_header_checksum(data + sizeof(eth_header)) ){
+			//printf_str("got a TCP packet port 80");
+			//printf_ip(" src ", htonl(p->ip.src)); 
+			//printf_str("\n"); 
+			//save it so we sent this guy data in the future.
+			ARP_lut_add(p->ip.src, p->eth.src); 
+			//check the checksum.
+			rxlen = tcp_length(p); 
+			printf_int(" client tcp len ",  rxlen); 
+			printf_str("\n"); 
+			if(tcp_checksum_check(data, rxlen)){
+				//now, must decide what to do with this packet. 
+				if(p->tcp.flags == TCP_FLAG_RST){
+					printf_str("TCP RST\n"); 
+					TcpState = TCP_LISTEN; 
+					TcpSeqClient = htonl(p->tcp.seq); 
+					g_httpRxed = 0; //reset the incoming buffer. 
+					return 1; 
+				}
+				if( p->tcp.flags == TCP_FLAG_SYN ){
+					printf_str("TCP got SYN"); 
+					TcpSeqClient = htonl(p->tcp.seq); 
+					TcpSeqClient += tcp_length(p); 
+					TcpSeqClient++; //that's the arbitration protocol
+					TcpClientPort = p->tcp.src;
+					g_httpRxed = 0; //reset the incoming buffer. 
+					//set up a response.
+					NetDestIP = p->ip.src; 
+					tcp_packet_setup(0, &result, NetDestIP, 
+						TCP_FLAG_SYN | TCP_FLAG_ACK, TcpSeqHost, TcpSeqClient); 
+					if(result > 0){
+						TcpSeqHost++; 
+						TcpState = TCP_SYN_RXED; 
+						//checksum. 
+						tcp_checksum_set(0); //no payload.
+						bfin_EMAC_send_nocopy();
+						return 1;
+					}					
+				}
+				if(p->tcp.flags == TCP_FLAG_ACK && TcpState == TCP_SYN_RXED){
+					if(htonl(p->tcp.ack) != TcpSeqHost){
+						printf_ip("TCP got unexpected ack, ", htonl(p->tcp.ack)); 
+						TcpState = TCP_LISTEN; 
+						printf_str("\n"); 
+						return 1; 
+					}
+					TcpState = TCP_CONNECTED; 
+					TcpSeqClient = htonl(p->tcp.seq); 
+					printf_str("TCP conneted\n"); 
+					return 1; 
+				}
+				if(p->tcp.flags & TCP_FLAG_FIN ){
+					printf_str("TCP got FIN\n"); 
+					TcpSeqClient = htonl(p->tcp.seq); 
+					NetDestIP = p->ip.src; 
+					TcpSeqClient++; 
+					if( TcpState == TCP_CONNECTED || TcpState == TCP_LISTEN ) {
+						tcp_packet_setup(0, &result, NetDestIP,
+							TCP_FLAG_FIN | TCP_FLAG_ACK, TcpSeqHost, TcpSeqClient); 
+						if(result > 0){
+							TcpState = TCP_CLOSING; 
+							tcp_checksum_set(0); //no payload.
+							bfin_EMAC_send_nocopy();
+							return 1; 
+						}
+					}
+					if( TcpState == TCP_CLOSING ) {
+						g_httpRxed = 0; //reset the incoming buffer. 
+						tcp_packet_setup(0, &result, NetDestIP,
+							 TCP_FLAG_ACK, TcpSeqHost, TcpSeqClient); //final ack.
+						if(result > 0){
+							TcpState = TCP_CLOSE; 
+							tcp_checksum_set(0); //no payload.
+							bfin_EMAC_send_nocopy();
+							return 1; 
+						}
+					}
+				}
+				if(p->tcp.flags & TCP_FLAG_ACK ){
+					if(TcpState == TCP_CLOSE){
+						printf_str("TCP got random ACK\n"); 
+						//do nothing .. !
+						return 1; 
+					}
+					if(TcpState == TCP_LISTEN){
+						//oops, the other side thinks we have a connection 
+						printf_str("TCP confused, sending RST\n"); //confused! 
+						g_httpRxed = 0; //reset the incoming buffer. 
+						TcpSeqClient = htonl(p->tcp.seq); 
+						NetDestIP = p->ip.src; 
+						TcpClientPort = p->tcp.src;
+						//TcpSeqClient++; 
+						tcp_packet_setup(0, &result, NetDestIP,
+								TCP_FLAG_RST, TcpSeqClient, 0); 
+						if(result > 0){
+							TcpState = TCP_LISTEN; 
+							tcp_checksum_set(0); //no payload.
+							bfin_EMAC_send_nocopy();
+							return 1; 
+						}
+					}
+				}
+				if(TcpState == TCP_CONNECTED){
+					//they are requesting something? 
+					payload = (char*)( &(p->tcp) ); 
+					payload += (p->tcp.dataoff & 0xf0)/4; //divide by 4 b/c it is << 4 & specifies 32 bit words. 
+					TcpSeqClient = htonl(p->tcp.seq); 
+					TcpSeqClient += rxlen; 
+					NetDestIP = p->ip.src; 
+					//start sending from whatever they acked last. 
+					u32 offset = htonl(p->tcp.ack) - TcpSeqHttpStart; 
+					//see if this was just an ack - if so, and we have more data to send, then send it. 
+					if(p->tcp.flags & TCP_FLAG_ACK && rxlen == 0){
+						//see if we have more data to send. 
+						if(offset == g_httpHeaderLen + g_httpContentLen){
+							g_httpRxed = 0; //reset the input counter. 
+							TcpSeqHost = offset + TcpSeqHttpStart; 
+							return 1; //nothing to send, everything acked.
+						} else {
+							if(tcp_burst(1, offset)) {
+								return 1; 
+							} else {
+								//nothing left - that was just an ack, wait for something else.
+								//this is the keepalive mode of connection.
+								return 1; 
+							}
+						}
+					}
+					if( httpCollate(payload, rxlen) ){
+						//ok, we have assembled a response - send or start sending it. 
+						TcpSeqHttpStart = TcpSeqHost; 
+						printf_int("sending http data length ",g_httpHeaderLen + g_httpContentLen); 
+						printf_str("\n"); 
+						tcp_burst(1, 0); 
+						return 1; 
+					} else {
+						//well, couldn't do anything with that packet - ack it, maybe they'll send another.
+						tx = tcp_packet_setup(0, &result, NetDestIP, TCP_FLAG_ACK ,
+							TcpSeqHost, TcpSeqClient); //keepalive function.
+						if(result > 0){
+							tcp_checksum_set(0); 
+							bfin_EMAC_send_nocopy();
+							return 1; 
+						}
+					}
+				}
+			}
+		}
+	}
+	return 0; 
+}
+int tcp_burst(int iter, u32 offset){
+	int i = 0; 
+	int txlen;
+	char result; 
+	u8* tx; 
+	int totalLen = g_httpHeaderLen + g_httpContentLen; 
+	while( i < iter && offset < totalLen ){
+		int remainingLen = totalLen - offset ; 
+		txlen = remainingLen > 1024 ? 1024 : remainingLen;
+		TcpSeqHost = offset + TcpSeqHttpStart;
+		tx = tcp_packet_setup(txlen, &result, NetDestIP,  
+				TCP_FLAG_ACK | TCP_FLAG_PSH, //don't close the connection.
+				TcpSeqHost, TcpSeqClient); 
+		if(result > 0){
+			u8* src;
+			int txhdrlen = 0; 
+			if(offset < g_httpHeaderLen){
+				src = (u8*)HTTP_HEADER; 
+				src += offset; 
+				txhdrlen = g_httpHeaderLen - offset; 
+				memcpy_(src, tx, txhdrlen); 
+				tx += txhdrlen ; 
+				txlen -= txhdrlen; 
+				offset += txhdrlen;  
+			}
+			src = (u8*)HTTP_CONTENT; 
+			src += offset - g_httpHeaderLen; 
+			memcpy_(src, tx, txlen); 
+			offset += txlen; 
+			TcpSeqHost = offset + TcpSeqHttpStart;
+			tcp_checksum_set(txlen + txhdrlen); 
+			bfin_EMAC_send_nocopy();
+			i++; 
+			if( (rxbuf[rxIdx]->StatusWord & RX_COMP) && (rxbuf[rxIdx]->StatusWord & RX_OK) ) {
+				//we break out if another packet has been received -- so that we can take care of it.
+				// (it may be an ack to our data).  
+				i += iter; 
+			}
+		}
+	}
+	return i; 
+}
