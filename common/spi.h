1b7a23a103ae935252d8b896af63cbf82779a62e
sideskate <sideskate@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2010-11-10 22:58:31 +0000

    got the radio working - the fifo needs to be cleared immediately after transitioning to RX / TX mode, and you have to be ready to immediately read it. there still seems to be data corruption issues / lag issues in the client program, will work on this later.

--- common/spi.h
+++ common/spi.h
@@ -64,7 +64,7 @@
 //SPI pins, needed by spi.c
 #define SPI_IRQ	0x0800 //port G pin 11
 #define SPI_CSN	0x0200 //port G pin 9
-#define SPI_CE	0x0400 //port G pin 10
+#define SPI_CE		0x0400 //port G pin 10
 
 #define SPI_BAUDDIV 6
 // system clock is 125Mhz, so divide by 12. (10.41MHz)
