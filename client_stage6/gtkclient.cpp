f11537df5a1dbfe4e6adc9564097a2e323d3effe
sideskate <sideskate@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2010-12-02 23:39:52 +0000

    working on a gtk version of the client -- more flexibility (actual gui elements!!)

---
+++ client_stage6/gtkclient.cpp
@@ -0,0 +1,294 @@
+/*
+ *
+ * Cribbed from an example Written by Davyd Madeley <davyd@madeley.id.au> and made available under a
+ * BSD license.
+ *
+ * This is purely an example, it may eat your cat and you can keep both pieces.
+ *
+ * to compile, see Makefile
+ */
+
+#include <gtk/gtk.h>
+#include <gtk/gtkgl.h>
+#include <GL/gl.h>
+
+#include <math.h>
+
+float boxv[][3] = {
+	{ -0.5, -0.5, -0.5 },
+	{  0.5, -0.5, -0.5 },
+	{  0.5,  0.5, -0.5 },
+	{ -0.5,  0.5, -0.5 },
+	{ -0.5, -0.5,  0.5 },
+	{  0.5, -0.5,  0.5 },
+	{  0.5,  0.5,  0.5 },
+	{ -0.5,  0.5,  0.5 }
+};
+#define ALPHA 0.5
+
+static float ang = 30.;
+
+static gboolean
+expose (GtkWidget *da, GdkEventExpose *event, gpointer user_data)
+{
+	GdkGLContext *glcontext = gtk_widget_get_gl_context (da);
+	GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (da);
+
+	// g_print (" :: expose\n");
+
+	if (!gdk_gl_drawable_gl_begin (gldrawable, glcontext))
+	{
+		g_assert_not_reached ();
+	}
+
+	/* draw in here */
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	
+	glRotatef (ang, 1, 0, 1);
+	// glRotatef (ang, 0, 1, 0);
+	// glRotatef (ang, 0, 0, 1);
+
+	glShadeModel(GL_FLAT);
+
+#if 0
+	glBegin (GL_QUADS);
+	glColor4f(0.0, 0.0, 1.0, ALPHA);
+	glVertex3fv(boxv[0]);
+	glVertex3fv(boxv[1]);
+	glVertex3fv(boxv[2]);
+	glVertex3fv(boxv[3]);
+
+	glColor4f(1.0, 1.0, 0.0, ALPHA);
+	glVertex3fv(boxv[0]);
+	glVertex3fv(boxv[4]);
+	glVertex3fv(boxv[5]);
+	glVertex3fv(boxv[1]);
+	
+	glColor4f(0.0, 1.0, 1.0, ALPHA);
+	glVertex3fv(boxv[2]);
+	glVertex3fv(boxv[6]);
+	glVertex3fv(boxv[7]);
+	glVertex3fv(boxv[3]);
+	
+	glColor4f(1.0, 0.0, 0.0, ALPHA);
+	glVertex3fv(boxv[4]);
+	glVertex3fv(boxv[5]);
+	glVertex3fv(boxv[6]);
+	glVertex3fv(boxv[7]);
+	
+	glColor4f(1.0, 0.0, 1.0, ALPHA);
+	glVertex3fv(boxv[0]);
+	glVertex3fv(boxv[3]);
+	glVertex3fv(boxv[7]);
+	glVertex3fv(boxv[4]);
+	
+	glColor4f(0.0, 1.0, 0.0, ALPHA);
+	glVertex3fv(boxv[1]);
+	glVertex3fv(boxv[5]);
+	glVertex3fv(boxv[6]);
+	glVertex3fv(boxv[2]);
+
+	glEnd ();
+#endif
+
+	glBegin (GL_LINES);
+	glColor3f (1., 0., 0.);
+	glVertex3f (0., 0., 0.);
+	glVertex3f (1., 0., 0.);
+	glEnd ();
+	
+	glBegin (GL_LINES);
+	glColor3f (0., 1., 0.);
+	glVertex3f (0., 0., 0.);
+	glVertex3f (0., 1., 0.);
+	glEnd ();
+	
+	glBegin (GL_LINES);
+	glColor3f (0., 0., 1.);
+	glVertex3f (0., 0., 0.);
+	glVertex3f (0., 0., 1.);
+	glEnd ();
+
+	glBegin(GL_LINES);
+	glColor3f (1., 1., 1.);
+	glVertex3fv(boxv[0]);
+	glVertex3fv(boxv[1]);
+	
+	glVertex3fv(boxv[1]);
+	glVertex3fv(boxv[2]);
+	
+	glVertex3fv(boxv[2]);
+	glVertex3fv(boxv[3]);
+	
+	glVertex3fv(boxv[3]);
+	glVertex3fv(boxv[0]);
+	
+	glVertex3fv(boxv[4]);
+	glVertex3fv(boxv[5]);
+	
+	glVertex3fv(boxv[5]);
+	glVertex3fv(boxv[6]);
+	
+	glVertex3fv(boxv[6]);
+	glVertex3fv(boxv[7]);
+	
+	glVertex3fv(boxv[7]);
+	glVertex3fv(boxv[4]);
+	
+	glVertex3fv(boxv[0]);
+	glVertex3fv(boxv[4]);
+	
+	glVertex3fv(boxv[1]);
+	glVertex3fv(boxv[5]);
+	
+	glVertex3fv(boxv[2]);
+	glVertex3fv(boxv[6]);
+	
+	glVertex3fv(boxv[3]);
+	glVertex3fv(boxv[7]);
+	glEnd();
+
+	glPopMatrix ();
+
+	if (gdk_gl_drawable_is_double_buffered (gldrawable))
+		gdk_gl_drawable_swap_buffers (gldrawable);
+
+	else
+		glFlush ();
+
+	gdk_gl_drawable_gl_end (gldrawable);
+
+	return TRUE;
+}
+
+static gboolean
+configure (GtkWidget *da, GdkEventConfigure *event, gpointer user_data)
+{
+	GdkGLContext *glcontext = gtk_widget_get_gl_context (da);
+	GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (da);
+
+	if (!gdk_gl_drawable_gl_begin (gldrawable, glcontext))
+	{
+		g_assert_not_reached ();
+	}
+
+	glLoadIdentity();
+	glViewport (0, 0, da->allocation.width, da->allocation.height);
+	/*printf("allocation.width %d allocation_height %d\n", 
+		da->allocation.width, da->allocation.height); */
+	glOrtho (-10,10,-10,10,-20050,10000);
+	glEnable (GL_BLEND);
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glScalef (10., 10., 10.);
+	
+	gdk_gl_drawable_gl_end (gldrawable);
+
+	return TRUE;
+}
+
+//global labels.. 
+GtkWidget* g_gainlabel[32];
+
+static gboolean
+rotate (gpointer user_data)
+{
+	char str[256]; 
+	GtkWidget *da = GTK_WIDGET (user_data);
+
+	ang++;
+
+	gdk_window_invalidate_rect (da->window, &da->allocation, FALSE);
+	gdk_window_process_updates (da->window, FALSE);
+
+	snprintf(str, 256, "%f", sin(ang)*4.0); 
+	gtk_label_set_text(GTK_LABEL(g_gainlabel[0]), str); //works!
+	return TRUE;
+}
+
+int
+main (int argc, char **argv)
+{
+	GtkWidget *window;
+	GtkWidget *da;
+	GdkGLConfig *glconfig;
+	//GtkWidget *table; 
+	GtkWidget *box1, *box2;
+	GtkWidget *button;
+	int i; 
+
+	gtk_init (&argc, &argv);
+	gtk_gl_init (&argc, &argv);
+
+	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
+	da = gtk_drawing_area_new ();
+
+	/* Create a 2x2 table */
+	//table = gtk_table_new (2, 2, TRUE);
+	box1 = gtk_vbox_new (FALSE, 0);
+	for(i=0; i<32; i++){
+		g_gainlabel[i] = gtk_label_new ("gain :: 4.0");
+		/* Align the label to the left side.  We'll discuss this function and 
+		 * others in the section on Widget Attributes. */
+		gtk_misc_set_alignment (GTK_MISC (g_gainlabel[i]), 0, 0);
+
+		/* Pack the label into the vertical box (vbox box1).  Remember that 
+		 * widgets added to a vbox will be packed one on top of the other in
+		 * order. */
+		gtk_box_pack_start (GTK_BOX (box1), g_gainlabel[i], FALSE, FALSE, 0);
+
+		/* Show the label */
+		gtk_widget_show (g_gainlabel[i]);
+	}
+	gtk_widget_show (box1);
+	box2 = gtk_hbox_new(FALSE,0); 
+	gtk_box_pack_start (GTK_BOX (box2), box1, FALSE, FALSE, 0);
+	gtk_box_pack_start (GTK_BOX (box2), da, TRUE, TRUE, 0);
+	gtk_widget_show (box2);
+	//gtk_widget_show (da);
+
+	
+	gtk_container_add (GTK_CONTAINER (window), box2);
+	/* Create first button */
+	//button = gtk_button_new_with_label ("button 1");
+	/* Insert button 1 into the upper left quadrant of the table */
+	//gtk_table_attach_defaults (GTK_TABLE (table), button, 0, 1, 0, 1);
+
+	//gtk_table_attach_defaults (GTK_TABLE (table), da, 0, 2, 1, 2);
+
+	g_signal_connect_swapped (window, "destroy",
+			G_CALLBACK (gtk_main_quit), NULL);
+	gtk_widget_set_events (da, GDK_EXPOSURE_MASK);
+
+	gtk_widget_show (window);
+
+	/* prepare GL */
+	glconfig = gdk_gl_config_new_by_mode (GdkGLConfigMode(
+			GDK_GL_MODE_RGB |
+			GDK_GL_MODE_DEPTH |
+			GDK_GL_MODE_DOUBLE));
+
+	if (!glconfig)
+	{
+		g_assert_not_reached ();
+	}
+
+	if (!gtk_widget_set_gl_capability (da, glconfig, NULL, TRUE,
+				GDK_GL_RGBA_TYPE))
+	{
+		g_assert_not_reached ();
+	}
+
+	g_signal_connect (da, "configure-event",
+			G_CALLBACK (configure), NULL);
+	g_signal_connect (da, "expose-event",
+			G_CALLBACK (expose), NULL);
+
+	gtk_widget_show_all (window);
+
+	g_timeout_add (1000 / 60, rotate, da);
+
+	gtk_main ();
+}
