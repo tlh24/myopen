682a898e6433b5e23380b120ba56029ceea912dc
sideskate <sideskate@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2010-12-03 00:22:40 +0000

    man gtk is great!!

--- trunk/client_stage6/gtkclient.cpp
+++ trunk/client_stage6/gtkclient.cpp
@@ -212,21 +212,30 @@ main (int argc, char **argv)
 {
 	GtkWidget *window;
 	GtkWidget *da;
+	GtkWidget *da_spikes;
 	GdkGLConfig *glconfig;
 	//GtkWidget *table; 
 	GtkWidget *box1, *box2;
 	GtkWidget *button;
+	GtkWidget *paned;
+	GtkWidget *paned2;
 	int i; 
 
 	gtk_init (&argc, &argv);
 	gtk_gl_init (&argc, &argv);
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title (GTK_WINDOW (window), "gtk headstage v6 client");
 	gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
 	da = gtk_drawing_area_new ();
+	gtk_widget_set_size_request(GTK_WIDGET(da), 600, 450);
+	da_spikes = gtk_drawing_area_new ();
+	gtk_widget_set_size_request(GTK_WIDGET(da_spikes), 600, 150);
 
 	/* Create a 2x2 table */
 	//table = gtk_table_new (2, 2, TRUE);
+	paned = gtk_hpaned_new(); 
+	gtk_container_add (GTK_CONTAINER (window), paned);
 	box1 = gtk_vbox_new (FALSE, 0);
 	for(i=0; i<32; i++){
 		g_gainlabel[i] = gtk_label_new ("gain :: 4.0");
@@ -242,15 +251,21 @@ main (int argc, char **argv)
 		/* Show the label */
 		gtk_widget_show (g_gainlabel[i]);
 	}
+	gtk_widget_set_size_request(GTK_WIDGET(box1), 200, 600);
 	gtk_widget_show (box1);
-	box2 = gtk_hbox_new(FALSE,0); 
-	gtk_box_pack_start (GTK_BOX (box2), box1, FALSE, FALSE, 0);
-	gtk_box_pack_start (GTK_BOX (box2), da, TRUE, TRUE, 0);
-	gtk_widget_show (box2);
+	
+	paned2 = gtk_vpaned_new(); 
+	gtk_paned_add1(GTK_PANED(paned2), da);
+	gtk_paned_add2(GTK_PANED(paned2), da_spikes); 
+	gtk_widget_show(paned2); 
+	
+	gtk_paned_add1(GTK_PANED(paned), box1);
+	gtk_paned_add2(GTK_PANED(paned), paned2); 
+	//gtk_box_pack_start (GTK_BOX (box2), box1, FALSE, FALSE, 0);
+	//gtk_box_pack_start (GTK_BOX (box2), da, TRUE, TRUE, 0);
+	gtk_widget_show (paned);
 	//gtk_widget_show (da);
 
-	
-	gtk_container_add (GTK_CONTAINER (window), box2);
 	/* Create first button */
 	//button = gtk_button_new_with_label ("button 1");
 	/* Insert button 1 into the upper left quadrant of the table */
