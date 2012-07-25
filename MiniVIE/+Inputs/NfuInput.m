8859bf22d13fd5ccbd69ed7c59ede0ae1eb9a01a
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2012-07-25 22:42:01 +0000

    moved udp functions centrally to NfuUdp class

--- MiniVIE/+Inputs/NfuInput.m
+++ MiniVIE/+Inputs/NfuInput.m
@@ -1,13 +1,22 @@
 classdef NfuInput < Inputs.CpcHeadstage
     % Class for interfacing JHU/APL NFU to MiniVIE.
+    %
+    % This is focused on the streaming data from the CPC Headstage which is
+    % a traitional daq channel Input block.  Other streaming types are
+    % percepts, vulcanx, algorithm, etc. however these will be handled by
+    % the NfuUdp object which holds a singleton interface to the Nfu Udp
+    % stream
+    %
     % 1Jan2012 Armiger: Created
     properties
-        EnableDataLogging = 0;
+        EnableDataLogging = false;
+        EnableHPF = false;
     end
     properties (SetAccess = private)
         hNfu            % Handle to NFU Comms Object
         hLogFile        % Handle to optional log file
         dataBuffer      % internal data buffer
+        
     end
     methods
         function obj = NfuInput()
@@ -19,81 +28,85 @@ classdef NfuInput < Inputs.CpcHeadstage
         function initialize(obj)
             
             obj.hNfu = MPL.NfuUdp.getInstance;
-            obj.hNfu.initialize();
-            obj.hNfu.enableStreaming(1);
-            
+            obj.hNfu.initialize();  % streaming begins here
+
         end
-        function data = getFilteredData(obj)
-            % Temp = make the NFU agnostic to filter settings since this is
-            % done in hardware
-            %data = getData(obj);
-            
-            data = getFilteredData@Inputs.SignalInput(obj);
-            
-        end
+% RSA: disabled this function.  superclass method will be called        
+%         function data = getFilteredData(obj)
+%             % Temp = make the NFU agnostic to filter settings since this is
+%             % done in hardware
+%             %data = getData(obj);
+%             
+%             data = getFilteredData@Inputs.SignalInput(obj);
+%             
+%         end
         function data = getData(obj)
             % This function will always return the correct size for data
             % (based on the number of samples) however results will be
             % padded with zeros.  User should check obj.AnalogInput.SamplesAvailable
             % for a deterministic result
-            len = 1;
-            while(len > 0)
-                len = pnet(obj.hNfu.UdpSocket,'readpacket','noblock');
-                if len > 0
-                    stream = pnet(obj.hNfu.UdpSocket,'read',len,'UINT8');
-                    
-                    numPacketHeaderBytes = 6;
-                    numSamplesPerPacket = 20;
-                    numSampleHeaderBytes = 4;
-                    numChannelsPerPacket = 8;
-                    numBytesPerChannel = 2;
-                    numBytesPerSample = numChannelsPerPacket*numBytesPerChannel + numSampleHeaderBytes;
-                    packetSize = numPacketHeaderBytes+numBytesPerSample*numSamplesPerPacket;
-                    if len ~= packetSize
-                        error('Unexpected Packet Size: Expected %d, Received %d\n',packetSize,len);
-                    end
-                    
-                    % First 6 bytes are global header
-                    data = reshape(stream(numPacketHeaderBytes+1:packetSize),numBytesPerSample,numSamplesPerPacket);
-                    % First 5 bytes per sample are header
-                    databytes = data(5:end,:);
-                    convertedFrame = reshape(typecast(databytes(:),'int16'),numChannelsPerPacket,numSamplesPerPacket);
-                    
-                    try
-                        if obj.EnableDataLogging
-                            if isempty(obj.hLogFile)
-                                fname = sprintf('%04d%02d%02d_%02d%02d%02d_NFU_Stream.log',fix(clock));
-                                obj.hLogFile = fopen(fname,'w+');
-                            end
-                            %fwrite(obj.hLogFile,now,'double');
-                            fwrite(obj.hLogFile,convertedFrame,'int16');
-                        else
-                            if ~isempty(obj.hLogFile)
-                                fclose(obj.hLogFile);
-                                obj.hLogFile = [];
-                            end
+            
+            % Determine expected packet size
+            numPacketHeaderBytes = 6;
+            numSamplesPerPacket = 20;
+            numSampleHeaderBytes = 4;
+            numChannelsPerPacket = 8;
+            numBytesPerChannel = 2;
+            numBytesPerSample = numChannelsPerPacket*numBytesPerChannel + numSampleHeaderBytes;
+            cpchpacketSize = numPacketHeaderBytes+numBytesPerSample*numSamplesPerPacket;
+
+            obj.hNfu.update();  % read available packets
+            
+            cellData = obj.hNfu.get_buffer(1);
+            
+            % Loop through all available packets and find cpch data
+            for i = 1:length(cellData)
+                % TODO verify packet order
+                stream = cellData{i};
+                
+                
+                % First 6 bytes of messgae are global header
+                data = reshape(stream(numPacketHeaderBytes+1:cpchpacketSize),numBytesPerSample,numSamplesPerPacket);
+                
+                % First 5 bytes per sample are header
+                databytes = data(5:end,:);
+                convertedFrame = reshape(typecast(databytes(:),'int16'),numChannelsPerPacket,numSamplesPerPacket);
+                
+                try
+                    if obj.EnableDataLogging
+                        if isempty(obj.hLogFile)
+                            fname = sprintf('%04d%02d%02d_%02d%02d%02d_NFU_Stream.log',fix(clock));
+                            obj.hLogFile = fopen(fname,'w+');
                         end
-                    catch ME
-                       fprintf('[%s] Error Logging: %s\n',ME.message); 
+                        %fwrite(obj.hLogFile,now,'double');
+                        fwrite(obj.hLogFile,convertedFrame,'int16');
+                    else
+                        if ~isempty(obj.hLogFile)
+                            fclose(obj.hLogFile);
+                            obj.hLogFile = [];
+                        end
                     end
-                    
-                    % Check for error blips
-                    if any(abs(convertedFrame(:)) > 10000)
-                        fprintf(2,'[%s] Bad Sample\n',mfilename);
-                    end
-                    
-                    % Poor mans HPF where we subtract off the mean value
-                    % over 20 samples from each sample
-                    convertedFrame = double(convertedFrame);
-                    convertedFrame = bsxfun(@minus,convertedFrame,mean(convertedFrame,2));
-                    
-                    % convertedFrame = mean(abs(convertedFrame));
-                    
-                    [numChannels numSamples] = size(convertedFrame);
-                    obj.dataBuffer = circshift(obj.dataBuffer,[0 -numSamples]);
-                    
-                    obj.dataBuffer(1:numChannels,end-numSamples+1:end) = convertedFrame;
+                catch ME
+                    fprintf('[%s] Error Logging: %s\n',ME.message);
                 end
+                
+                % Check for error blips
+                if any(abs(convertedFrame(:)) > 10000)
+                    fprintf(2,'[%s] Bad Sample\n',mfilename);
+                end
+                
+                % Poor mans HPF where we subtract off the mean value
+                % over 20 samples from each sample
+                convertedFrame = double(convertedFrame);
+                if obj.EnableHPF
+                    convertedFrame = bsxfun(@minus,convertedFrame,mean(convertedFrame,2));
+                % convertedFrame = mean(abs(convertedFrame));
+                end
+                
+                [numChannels numSamples] = size(convertedFrame);
+                obj.dataBuffer = circshift(obj.dataBuffer,[0 -numSamples]);
+                
+                obj.dataBuffer(1:numChannels,end-numSamples+1:end) = convertedFrame;
             end
             
             dataBuff = obj.dataBuffer(:,end-obj.NumSamples+1:end)';
