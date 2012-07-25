1ca3513e72295f02c9046ed60b5fd16676ea1b6e
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2012-07-25 22:45:24 +0000

    updated Nfu Comms object for handling different message types

--- MiniVIE/+MPL/NfuUdp.m
+++ MiniVIE/+MPL/NfuUdp.m
@@ -1,45 +1,70 @@
 classdef (Sealed) NfuUdp < handle
     % Class for interfacing JHU/APL NFU via pnet.
     % Note instances are limited so use NfuUdp.getInstance
+    %
+    % This handles streaming of all data types (cpch, percepts, etc.)
+    %
+    % Usage:
+    % hNfu = MPL.NfuUdp.getInstance;
+    % hNfu.initialize();
+    %
+    % hNfu.setParam(NFU_run_algorithm,0)  %% 0 implies algorithm runs on laptop
+    % hNfu.setParam(NFU_output_to_MPL,2)  %% 2 = NFU CAN to limb
+    %
+    % Note data streams start in the initialize function
+    %
     properties
-        
-        hNFU = []; % Handle to NFU Comms Object
-        
-        Host = '10.3.1.11';
+                
+        %Host = '10.3.1.11';
         UdpPortNum = 9027
         TcpPortNum = 6200;
-        
-        TcpConnection = [];
-        
+                
         EnableDataLogging = 0;
+        
+        UdpBuffer1
+        UdpBuffer2
+        UdpBuffer3
+        UdpBuffer4
         
         hLogFile
         
     end
     properties (SetAccess = private)
         IsInitialized = false;
-        UdpSocket = [];
+
+        TcpConnection = [];
         TcpSocket = [];
+        UdpSocket = [];
+        
+        BufferSize = 100;
+        
+        newData1
+        newData2
+        newData3
+        newData4
+        
+        ptr1
+        ptr2
+        ptr3
+        ptr4
+        
+        sum1
+        sum2
+        sum3
+        sum4
+        
+        numPacketsReceived = 0;
+        
     end
     methods (Access = private)
         function obj = NfuUdp
             % Creator is private to force singleton
-        end
-    end
-    methods (Static)
-        function singleObj = getInstance
-            persistent localObj
-            if isempty(localObj) || ~isvalid(localObj)
-                fprintf('[%s] Calling constructor\n',mfilename);
-                localObj = MPL.NfuUdp;
-            else
-                fprintf('[%s] Returning existing object\n',mfilename);
-            end
-            singleObj = localObj;
+            reset_buffers(obj);
         end
     end
     methods
         function [ status ] = initialize(obj)
+            
             if obj.IsInitialized
                 fprintf('[%s] NFU Comms already initialized\n',mfilename);
                 status = 0;
@@ -50,14 +75,20 @@ classdef (Sealed) NfuUdp < handle
             pnet(obj.UdpSocket, 'setreadtimeout',0);
             
             % open a CMD connection to the NFU
-            fprintf( '[%s] Creating tcpsocket on port %d...', mfilename,obj.TcpPortNum);
+            fprintf( '[%s] Creating local tcpsocket on port %d...', mfilename,obj.TcpPortNum);
             obj.TcpSocket = pnet( 'tcpsocket', obj.TcpPortNum );
-            if (-1 ~= obj.TcpSocket)
+            socketSuccess = (-1 ~= obj.TcpSocket);
+            
+            if socketSuccess
                 fprintf('OK\n');
                 pnet( obj.TcpSocket, 'setreadtimeout', 1 );
+
+                % Try to crate connection to NFU.  Only works if NFU is
+                % ready to receive commands
                 fprintf( '[%s] tcplisten on tcpsocket...',mfilename);
                 obj.TcpConnection = pnet( obj.TcpSocket, 'tcplisten' );
-                if (-1 ~= obj.TcpConnection)
+                connectionSuccess = (-1 ~= obj.TcpConnection);
+                if connectionSuccess
                     fprintf('OK\n');
                     pnet( obj.TcpConnection, 'setreadtimeout', 1 );
                     
@@ -68,6 +99,8 @@ classdef (Sealed) NfuUdp < handle
                     %pnet( cmdCon, 'readline' );
                 else
                     fprintf('FAILED\n' );
+                    fprintf( '[%s] Closing tcpsocket...\n',mfilename);
+                    pnet(obj.TcpSocket, 'close' )
                     status = -1;
                     return
                 end
@@ -77,17 +110,40 @@ classdef (Sealed) NfuUdp < handle
                 return
             end
             status = 0;
+            
+            % StartStreams
+            
+            % Enable CPCH Data
+            fprintf('[%s] Enabling CPCH Data Stream\n',mfilename);
+            obj.enableStreaming(1);
+            
+            % Enable Percepts
+            fprintf('[%s] Enabling Percepts Data Stream\n',mfilename);
+            obj.enableStreaming(4);
+
             obj.IsInitialized = true;
+            
+            obj.enableRunMode;
+            
+        end
+        function enableRunMode(obj)
+            NFU_run_algorithm = evalin('base','NFU_run_algorithm');
+            obj.setParam(NFU_run_algorithm,0)  %% 0 implies algorithm runs on laptop
+
+            NFU_output_to_MPL = evalin('base','NFU_output_to_MPL');
+            obj.setParam(NFU_output_to_MPL,2)  %% 2 = NFU CAN to limb
+            
         end
         function enableStreaming(obj,type)
             if nargin < 2
                 type = 1;
             end
             
-            [ status ] = obj.disable_streaming( obj.TcpConnection, type );
-            if status
-                error('Failed to stop NFU streaming');
-            end
+            % With wireless, multiple streams are ok
+            %             [ status ] = obj.disable_streaming( obj.TcpConnection, type );
+            %             if status
+            %                 error('Failed to stop NFU streaming');
+            %             end
             
             [ status ] = obj.enable_streaming( obj.TcpConnection, type );
             if status
@@ -116,25 +172,169 @@ classdef (Sealed) NfuUdp < handle
             
         end
         function setParam(obj,param,value)
+            % Take input parameter and send to NFU.  if value is provided
+            % the new value will be updated.  If not provided, the value
+            % used from the base workspace parameter will be used
+            % if param is a string, it should be the name of a base
+            % workspace SimulinkParamter, otherwise, param should be the
+            % actual paramter
+            
+            if ischar(param)
+                % Input can be either the actual simulink parameter or a
+                % string corresponding to the parameter in the base
+                % workspace.
+                simulinkParam = evalin('base',param);
+            elseif isa(param,'Simulink.Parameter')
+                % input is the actual simulink parameter
+                simulinkParam = param;
+            else
+                error('Unknown data type passed into input parameter');
+            end
+            
             % If value not passed, update param with the .Value field
             if nargin > 2
-                if ~isequal(size(value),param.Dimensions)
+                if ~isequal(size(value),simulinkParam.Dimensions)
                     error('Bad Value Size');
                 end
                 
-                param.Value = value;
+                simulinkParam.Value = value;
             end
-            [ status, msg ] = obj.update_param( obj.TcpConnection, param );
+            
+            fprintf('[%s] Setting NFU parameter %s to %d\n',mfilename,simulinkParam.Description,value);
+            
+            [ status, msg ] = obj.update_param( obj.TcpConnection, simulinkParam );
+
             if status
-                fprintf(2,'Failed to update parameter: %s',param.Description);
+                fprintf(2,'Failed to update parameter: %s',simulinkParam.Description);
             end
             
         end
-        function mud = getMud(obj)
+        function reset_buffers(obj)
+            obj.BufferSize = 100;
+            
+            obj.UdpBuffer1 = cell(obj.BufferSize,1);
+            obj.UdpBuffer2 = cell(obj.BufferSize,1);
+            obj.UdpBuffer3 = cell(obj.BufferSize,1);
+            obj.UdpBuffer4 = cell(obj.BufferSize,1);
+            
+            obj.newData1 = false(obj.BufferSize,1);
+            obj.newData2 = false(obj.BufferSize,1);
+            obj.newData3 = false(obj.BufferSize,1);
+            obj.newData4 = false(obj.BufferSize,1);
+            
+            obj.ptr1 = 1;
+            obj.ptr2 = 1;
+            obj.ptr3 = 1;
+            obj.ptr4 = 1;
+            
+            obj.sum1 = 0;
+            obj.sum2 = 0;
+            obj.sum3 = 0;
+            obj.sum4 = 0;
+            
+        end
+        function tactorControl(obj, nodeid, period, amplitude, duration, currentLimit, offset )
+            % If value not passed, update param with the .Value field
+            [ status] = obj.tactor_control( obj.TcpConnection, nodeid, period, amplitude, duration, currentLimit, offset );
+            if status
+                fprintf(2,'Failed to send tactor control: %s',param.Description);
+            end
+            
+        end
+        
+        function cellData = get_buffer(obj,id)
+            % read unread buffer data and return array of cells
+            
+            switch id
+                case 1
+                    %fprintf('[%s] reading %d unread data packets\n',mfilename,sum(obj.newData1));
+                    
+                    idOfInterest = obj.ptr1:(obj.ptr1+obj.BufferSize-1);
+                    idOfInterest = idOfInterest(:) & obj.newData1(:);
+                    
+                    % wrap indices eg: 99 100 1 2 3
+                    idOfInterest(idOfInterest > obj.BufferSize) = idOfInterest(idOfInterest > obj.BufferSize) - obj.BufferSize;
+                    
+                    % return newest signals first
+                    %idx = flipud(find(idOfInterest));
+                    idx = find(idOfInterest);
+                    
+                    cellData = obj.UdpBuffer1(idx);
 
+                    obj.newData1(:) = false;
+
+                case 2
+                    %fprintf('[%s] reading %d unread data packets\n',mfilename,sum(obj.newData2));
+                    
+                    idOfInterest = obj.ptr2:(obj.ptr2+obj.BufferSize-1);
+                    idOfInterest = idOfInterest(:) & obj.newData2(:);
+                    
+                    % wrap indices eg: 99 100 1 2 3
+                    idOfInterest(idOfInterest > obj.BufferSize) = idOfInterest(idOfInterest > obj.BufferSize) - obj.BufferSize;
+                    
+                    % return newest signals first
+                    idx = flipud(find(idOfInterest));
+                    %idx = find(idOfInterest);
+                    
+                    cellData = obj.UdpBuffer2(idx);
+
+                    obj.newData2(:) = false;
+                    
+
+            end
+            
+        end
+        function update(obj)
+            % Update function reads any buffered udp packets and stores
+            % them for later use
+            
+            %UdpBuffer
+            len = 1;
+            while(len > 0)
+                len = pnet(obj.UdpSocket,'readpacket','noblock');
+                if len == 406
+                    % advance packet counter
+                    obj.numPacketsReceived = obj.numPacketsReceived + 1;
+
+                    % read data and mark as new
+                    obj.UdpBuffer1{obj.ptr1} = pnet(obj.UdpSocket,'read',len,'UINT8');
+                    obj.newData1(obj.ptr1) = true;
+                    
+                    % advance ptr
+                    obj.ptr1 = obj.ptr1 + 1;
+                    if obj.ptr1 > obj.BufferSize
+                        obj.ptr1 = 1;
+                    end
+                    
+                    obj.sum1 = obj.sum1 + 1;
+                elseif len == 131
+                    % advance packet counter
+                    obj.numPacketsReceived = obj.numPacketsReceived + 1;
+
+                    % read data and mark as new
+                    obj.UdpBuffer2{obj.ptr2} = pnet(obj.UdpSocket,'read',len,'UINT8');
+                    obj.newData2(obj.ptr2) = true;
+
+                    % advance ptr
+                    obj.ptr2 = obj.ptr2 + 1;
+                    if obj.ptr2 > obj.BufferSize
+                        obj.ptr2 = 1;
+                    end
+                    
+                    obj.sum2 = obj.sum2 + 1;
+                    
+                elseif len > 0
+                    error('Unexpected Packet Size.');
+                end
+            end
+            
+            
+        end
+        function mud = getMud(obj)
+            
             mud = [];
             len = 1;
-
+            
             while len > 0
                 len = pnet(obj.UdpSocket,'readpacket','noblock');
                 if len > 0
@@ -149,18 +349,28 @@ classdef (Sealed) NfuUdp < handle
             end
         end
         function close(obj)
-%             try
-%                 pnet(obj.UdpSocket,'close');
-%             end
-%             try
-%             pnet(obj.TcpConnection,'close');
-%             end
-%             try
-%             pnet(obj.TcpSocket,'close');
-%             end
+            %             try
+            %                 pnet(obj.UdpSocket,'close');
+            %             end
+            %             try
+            %             pnet(obj.TcpConnection,'close');
+            %             end
+            %             try
+            %             pnet(obj.TcpSocket,'close');
+            %             end
         end
     end
     methods (Static)
+        function singleObj = getInstance
+            persistent localObj
+            if isempty(localObj) || ~isvalid(localObj)
+                fprintf('[%s] Calling constructor\n',mfilename);
+                localObj = MPL.NfuUdp;
+            else
+                fprintf('[%s] Returning existing object\n',mfilename);
+            end
+            singleObj = localObj;
+        end
         function [ status ] = enable_streaming( pnet_conn, type )
             %update_param Summary of this function goes here
             %   Detailed explanation goes here
@@ -171,6 +381,14 @@ classdef (Sealed) NfuUdp < handle
             elseif type == 2
                 %VULCANX
                 enable_stream_Nfu = char( uint8(150), 9, 8, zeros(7,1));
+            elseif type == 3
+                %ALGORITHM
+                enable_stream_Nfu = char( uint8(150), 9, 4, zeros(7,1));
+            elseif type == 4
+                %LC PERCEPTS
+                enable_stream_Nfu = char( uint8(150), 10, 1, zeros(7,1));
+            else
+                error('Unmatched Type');
             end
             
             %pnet( pnet_conn, 'write', enable_stream_Nfu ); %write cfg NFU
@@ -187,6 +405,12 @@ classdef (Sealed) NfuUdp < handle
             elseif type == 2
                 %VIE
                 enable_stream_Nfu = char( uint8(150), 9, 0, zeros(7,1));
+            elseif type == 3
+                %ALGORITHM
+                enable_stream_Nfu = char( uint8(150), 9, 0, zeros(7,1));
+            elseif type == 4
+                %LC PERCEPTS
+                enable_stream_Nfu = char( uint8(150), 10, 0, zeros(7,1));
             end
             
             %pnet( pnet_conn, 'write', enable_stream_Nfu ); %write cfg NFU
@@ -226,7 +450,7 @@ classdef (Sealed) NfuUdp < handle
             
         end
         function [ status ] = send_msg( pnet_conn, msg )
-
+            
             % Clear read buffer of any existing data:
             reply = 0;
             while ~isempty(reply)
@@ -244,21 +468,37 @@ classdef (Sealed) NfuUdp < handle
             checkReceipt = false;
             
             if checkReceipt
-            try
-                reply = pnet(pnet_conn,'read',1,'uint8');
-            catch ME
-                fprintf(2,'[%s] Error: %s\n',mfilename,ME.message);
-            end
-            if reply == msgId
-                status = 0;
-            else
-                status = -1;
-                msg = sprintf('Failed to receive ACK on parameter update. Reply = "%d"\n',reply);
-            end
+                try
+                    reply = pnet(pnet_conn,'read',1,'uint8');
+                catch ME
+                    fprintf(2,'[%s] Error: %s\n',mfilename,ME.message);
+                end
+                if reply == msgId
+                    status = 0;
+                else
+                    status = -1;
+                    msg = sprintf('Failed to receive ACK on parameter update. Reply = "%d"\n',reply);
+                end
             else
                 status = 0;
             end
             
         end
+        function [ status ] = tactor_control( pnet_conn, nodeid, period, amplitude, duration, currentLimit, offset )
+            tactor_node = typecast(int32(nodeid), 'uint8');
+
+            per = uint8(period);
+            amp = typecast(int16(amplitude), 'uint8');
+            dur = typecast(uint16(duration), 'uint8');
+            curr = typecast(uint16(currentLimit), 'uint8');
+            off = typecast(int16(offset), 'uint8');
+
+
+            tactor_control_Nfu = char( 60, reshape(tactor_node, length(tactor_node),1), per, reshape(amp, length(amp),1), reshape(dur, length(dur),1), reshape(curr, length(curr),1), reshape(off, length(off),1));
+            %length(tactor_control_Nfu)
+            pnet( pnet_conn, 'write', tactor_control_Nfu );
+            [ status ] = MPL.NfuUdp.send_msg( pnet_conn, tactor_control_Nfu );
+            
+        end        
     end
 end
---
+++ MiniVIE/+MPL/TEST_MPL_NFU.m
@@ -0,0 +1,39 @@
+% Startup notes:  0) run cc / go (create simulink variables)
+
+
+%%
+hNfu = MPL.NfuUdp.getInstance;
+hNfu.initialize();
+
+%hNfu.setParam(NFU_run_algorithm,0)  %% 0 implies algorithm runs on laptop
+%hNfu.setParam(NFU_output_to_MPL,2)  %% 2 = NFU CAN to limb
+%%
+%Stream CPCH
+hNfu.enableStreaming(1);
+% Enlable Percepts
+hNfu.enableStreaming(4);
+
+%%
+pnet_conn = hNfu.TcpConnection;
+mud = MPL.MudCommandEncoder();
+%%
+w = [-15 0 0]*5*pi/180;
+rocPos = 0/10;
+rocID = 5;
+elbow = 5*5*pi/180;
+
+msg = mud.ArmPosVelHandRocGrasps([zeros(1,3) elbow w],zeros(1,7),1,rocID,rocPos,1);
+% pnet( hNfu.TcpConnection, 'write', char(59,msg)); %Upper arm and wrist DOM PV, ROC for hand
+hNfu.send_msg(hNfu.TcpConnection,char(59,msg));  % append nfu msg header
+
+%%
+tic
+while StartStopForm
+    w = [0 sin(toc) 0];
+    rocPos = .5;
+    rocID = 2;
+    
+    msg = mud.ArmPosVelHandRocGrasps([zeros(1,4) w],zeros(1,7),1,rocID,rocPos,1);
+    hNfu.send_msg(hNfu.TcpConnection,char(59,msg));
+    drawnow
+end
