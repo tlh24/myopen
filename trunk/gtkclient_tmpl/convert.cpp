0da63bb7c7a66ce2f7b7028e8589f2d801855bfe
zeraphil <zeraphil@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2012-09-14 19:40:39 +0000

    updated convert to decode strobe...whoops

--- trunk/gtkclient_tmpl/convert.cpp
+++ trunk/gtkclient_tmpl/convert.cpp
@@ -140,7 +140,9 @@ int main(int argn, char **argc){
 		u64 rxpackets = 0;
 		u64 txpackets = 0;
 		u64 msgpackets = 0;
+		u64 strobepackets = 0;
 		u64 msglength = 0;
+		u64 strobelength = 0;
 		u64 spikes = 0;
 		bool done = false;
 		while(!done){
@@ -184,6 +186,15 @@ int main(int argn, char **argc){
 					msglength += siz;
 					fseeko(in,siz+8, SEEK_CUR); //8 byte double timestamp.
 					pos += 16+siz;
+				}else if(u == 0x1eafbabe){
+					//tracking info.
+					fread((void*)&u,4,1,in);
+					unsigned int siz = u & 0xffff;
+					//printf("u 0x%x\n",u);
+					strobepackets += 1;
+					strobelength += siz + 8;
+					fseeko(in,siz+8, SEEK_CUR); //8 byte double timestamp.
+					pos += 16+siz;
 				} else {
 					printf("magic number seems off, is 0x%x, %lld bytes, %lld packets\n",
 						   u,pos,rxpackets);
@@ -192,8 +203,8 @@ int main(int argn, char **argc){
 				if(ferror(in) || feof(in)) done = true;
 			}
 		}
-		printf("total %lld rxpackets, %lld txpackets, %lld spikes, %lld messages\n",
-			   rxpackets, txpackets, spikes, msgpackets);
+		printf("total %lld rxpackets, %lld txpackets, %lld spikes, %lld messages, %lld strobepackets\n",
+			   rxpackets, txpackets, spikes, msgpackets, strobepackets);
 		if(rxpackets > 0x7fffffff){
 			printf("you will not be able to save packet timestamps.\n");
 		}
@@ -202,16 +213,20 @@ int main(int argn, char **argc){
 		// time (double), analog(i8), channel (i8),
 		// spike_time (double), spikes(i32)
 		double* time;
+		double* strobe_tx;
+		double* strobe_rx;
 		mat_uint32_t* mstimer;
 		unsigned char* analog;
 		unsigned char* channel;
 		mat_uint32_t* spike_ts;
 		mat_int8_t* spike_ch;
 		mat_int8_t* spike_unit;
+		mat_uint32_t*  track_cam;
 		// store timestamp (in samples), rx time (not necessarily accurate) - one per pkt
 		// store channel # and sample
 		// store channel & timestamp for spikes.
 		// just ignore dropped packets for now.
+			// store camera & timestamp for tracking
 		time = (double*)malloc(rxpackets * sizeof(double));
 		  if(!time){ printf("could not allocate time variable."); exit(0);}
 		mstimer = (mat_uint32_t*)malloc(rxpackets * sizeof(int) );
@@ -229,9 +244,20 @@ int main(int argn, char **argc){
 		channel = (unsigned char*)malloc(rxpackets * 4 ); //channel does not change within packets.
 		  if(!channel){ printf("could not allocate channel variable."); exit(0);}
 
+
+		strobe_tx = (double*)malloc(strobepackets * sizeof(double)); //client side timestamp
+			if(!strobe_tx){ printf("could not allocate tracking variable."); /*exit(0) We don't want to exit!;*/}
+		strobe_rx = (double*)malloc(strobepackets * sizeof(double)); //server side timestamp
+			if(!strobe_rx){ printf("could not allocate tracking variable."); /*exit(0) We don't want to exit!;*/}
+		track_cam  = (mat_uint32_t* )malloc(strobepackets * sizeof(int));
+			if(!track_cam){ printf("could not allocate tracking variable."); /*exit(0) We don't want to exit!;*/}
+			
+			
 		//also need to inspect the messages, to see exactly when the channels changed.
 		u64 tp = 0; // packet position (index time, aka timestamp)
 		u64 sp = 0; // spike position (index spike variables)
+		u64 kp = 0; // strobe packet position (index time, aka strobe timestamp)
+
 		char msgs[16][128]; //use this to save messages ; appy them when their echo appears.
 		for(u64 i=0; i<16*128; i++){
 			msgs[0][i] = 0; //yes, you can do that in c!
@@ -332,6 +358,33 @@ int main(int argn, char **argc){
 							//printf(" chan %d changed to %d\n", ii, chans[ii]);
 						}
 					}
+				} else if( u == 0x1eafbabe){
+					fread((void*)&u,4,1,in);
+					unsigned int siz = u & 0xffff;
+					double rxtime = 0.0;
+					fread((void*)&rxtime,8,1,in); //rx time in seconds.
+					
+					//read the buffer (format) "cam timstamp"
+					char buf[64];
+					//buf[siz] = 0;
+					fread((void*)&buf, siz, 1, in);
+					int cam;
+					double txtime;
+					sscanf(buf, "%d %lg", &cam, &txtime);
+					strobe_tx[kp] = txtime;
+					strobe_rx[kp] = rxtime;
+					track_cam[kp] = cam;
+					//fseeko(in,siz, SEEK_CUR);
+					pos += 16+siz;
+					
+					kp++;
+					if(kp > strobepackets){
+						printf("error! time position kp > strobepackets\n");
+						printf("%lld > %lld \n", kp, strobepackets);
+						printf("file offset %ld\n", ftello(in));
+						exit(0);
+					}
+
 					pos += 16+siz;
 				} else {
 					printf("magic number seems off, is 0x%x, %lld bytes\n",
@@ -349,16 +402,24 @@ int main(int argn, char **argc){
 			printf("error: sp %lld, too large for 32-bit integer, clipping data.\n", sp);
 			exit(0);
 		}
+		if(kp > 0x7fffffff){
+			printf("error: kp %lld, too large for 32-bit integer, clipping data.\n", kp);
+			exit(0);
+		}
+
 		//wrap them anyway.
 		int tpp = (int)(tp & 0x7fffffff);
 		int spp = (int)(sp & 0x7fffffff);
+		int kpp = (int)(kp & 0x7fffffff);
+
 		matvar_t *matvar;
 		matvar = Mat_VarCreate("time",MAT_C_DOUBLE,MAT_T_DOUBLE,
 							   1,&tpp,time,0);
 		Mat_VarWrite( mat, matvar, 0 );
 		Mat_VarFree(matvar);
 		free(time); //I wish I had more.
-
+		//Me too.
+		
 		matvar = Mat_VarCreate("mstimer",MAT_C_UINT32,MAT_T_UINT32,
 							   1,&tpp,mstimer,0);
 		Mat_VarWrite( mat, matvar, 0 );
@@ -382,6 +443,28 @@ int main(int argn, char **argc){
 		Mat_VarWrite( mat, matvar, 0 );
 		Mat_VarFree(matvar);
 		free(spike_unit);
+		
+		//put strobe sync in matlab
+		matvar = Mat_VarCreate("strobe_tx",MAT_C_DOUBLE,MAT_T_DOUBLE,
+							   1,&kpp,strobe_tx,0);
+		Mat_VarWrite( mat, matvar, 0 );
+		Mat_VarFree(matvar);
+		free(strobe_tx);
+		
+		//strobe reception timestamp
+		matvar = Mat_VarCreate("strobe_rx",MAT_C_DOUBLE,MAT_T_DOUBLE,
+							   1,&kpp,strobe_rx,0);
+		Mat_VarWrite( mat, matvar, 0 );
+		Mat_VarFree(matvar);
+		free(strobe_rx);
+		
+		//track cam
+		matvar = Mat_VarCreate("track_cam",MAT_C_UINT32,MAT_T_UINT32,
+							   1,&kpp,track_cam,0);
+		Mat_VarWrite( mat, matvar, 0 );
+		Mat_VarFree(matvar);
+		free(track_cam);
+
 
 		//most analog traces do not fit into 2 gigs -- write them out RAW.
 		printf("writing raw analog traces\n");
