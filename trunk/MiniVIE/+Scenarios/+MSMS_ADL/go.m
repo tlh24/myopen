b7c9b6bcbb8b45190211efbfe62a28a3d1af1f1e
rarmiger <rarmiger@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2012-12-21 16:43:27 +0000

    updated to WRNMMC Settings

--- trunk/MiniVIE/+Scenarios/+MSMS_ADL/go.m
+++ trunk/MiniVIE/+Scenarios/+MSMS_ADL/go.m
@@ -15,7 +15,7 @@ obj = Scenarios.MSMS_ADL.EmgAdl;
 % Comment out one of these lines to either interactively load or specify files
 % setMotionFile(obj);  % interactive load
 % setMotionFile(obj,'C:\MSMS\MSMS Beta 0.9.1\Sample Models\WRAMC_Model\Data');  %(exclude file extensions)
-setMotionFile(obj,'C:\MSMS\Sample Models\WRAMC_Model\Data','LeftArmRun1'); %(exclude file extensions)
+setMotionFile(obj,'C:\MSMS\Sample Models\WRAMC_Model\Data','LeftArm15'); %(exclude file extensions)
 
 initialize(obj)
 obj.ActiveChannels = [1:8 ]; % Note HW channels are 0-based
