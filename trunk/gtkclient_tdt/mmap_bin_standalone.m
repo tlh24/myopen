a8c33fda48c26f672b4c86ae33888e0fc5ef9f3b
sideskate@gmail.com <sideskate@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2013-04-16 19:51:50 +0000

    investigating binning bug.

--- trunk/gtkclient_tdt/mmap_bin_standalone.m
+++ trunk/gtkclient_tdt/mmap_bin_standalone.m
@@ -1,35 +1,28 @@
 % mmap memory.  works okay with the default (java) matlab mmap code. 
 m = memmapfile('/tmp/binned', 'Format', {'uint16' [10 194] 'x'}); 
 A = m.Data(1).x;
-n = 200; 
-global bmi5_out bmi5_in b5 gtkclient_out gtkclient_in
+n = 1e4; 
+global gtkclient_out gtkclient_in
 
 gtkclient_out = fopen('/tmp/gtkclient_out.fifo', 'r'); 
 gtkclient_in = fopen('/tmp/gtkclient_in.fifo', 'w'); 
-
-bmi5_out = fopen('/tmp/bmi5_out.fifo', 'r');
-bmi5_in  = fopen('/tmp/bmi5_in.fifo',  'w');
-
-eval(bmi5_cmd('mmap'));
 
 skip = 0;
 prev = 0;
 tic();
 for i=1:n
-	b5 = bmi5_mmap(b5); 
-	fwrite(gtkclient_in, b5.time_o - 0.4, 'double'); % sample then.
+	fwrite(gtkclient_in, -1, 'double'); %sample now.
 	msg = fread(gtkclient_out, 3, 'uchar');
 	if A(1,193) - prev ~= 1
 		skip = skip + 1; 
 	end
 	prev = A(1,193); 
-	if 1
+	if 0
 		disp([num2str(i) ' ' num2str(A(1,193))]); 
-		imagesc(A(:,1:192)/128); % units: spikes / bin.
+		imagesc(A(:,1:192)/128); 
 		ylabel('lag')
 		xlabel('neuron'); 
 		colormap gray
-        colorbar
 		drawnow
 	end
 	dat(:, i) = A(:, 1); 
@@ -43,6 +36,3 @@ fclose(gtkclient_in);
 fclose(gtkclient_out); 
 
 plot(dat'); 
-
-fclose(bmi5_in); 
-fclose(bmi5_out); 
