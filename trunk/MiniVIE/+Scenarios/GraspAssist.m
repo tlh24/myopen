f4774db50ef5a2bdce564b19942f8ee913118513
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2013-08-07 22:01:21 +0000

    added comments

--- trunk/MiniVIE/+Scenarios/GraspAssist.m
+++ trunk/MiniVIE/+Scenarios/GraspAssist.m
@@ -1,6 +1,13 @@
 classdef GraspAssist < Scenarios.OnlineRetrainer
     % Scenario for controlling grasp assist device
-    % 
+    %
+    %
+    %
+    %
+    % Demo configuration:
+    %   Run RCP device with MiniVIE EMG
+    %     Scenarios.GraspAssist.Run(mode)
+    %     mode == 1 :
     % 21-Apr-2013 Armiger: Created
     properties
         
@@ -11,6 +18,8 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
         hTopoState
         
         LastIntent
+        
+        ConnectRcp = false;
     end
     methods
         function obj = GraspAssist
@@ -23,22 +32,40 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
             
             
             % setup serial device
-            obj.hDevice
-            
+            if obj.ConnectRcp
+                comPort = 'COM12';
+                obj.hDevice = serialSetup(comPort);
+                fprintf('Connecting serial device on %s...\n',comPort)
+                
+                pause(2);
+                
+                % Home all
+                if strcmp('OK',questdlg('Ready to home?','Home All Actuators','OK','Cancel','OK'))
+                    disp('Home All');
+                    fprintf(obj.hDevice,string2Can('HA'));
+                    disp('[Press any key to continue]'),pause
+                end
+                
+            end
+            disp('Device setup Complete');
             
             obj.hTopoState = Scenarios.TopoGrasp();
             
             obj.hTopoState.setup_display();
-
+            
             isLeftSide = 1;
             obj.hTopoState.hHandState = Controls.HandStatePointer;
-
+            
             obj.hTopoState.hLegend = Controls.HandStateLegend(obj.hTopoState.hHandState,isLeftSide);
             obj.hTopoState.hLegend.EnableStateChangeBeep = 1;
             
             
             % Remaining superclass initialize methods
             initialize@Scenarios.OnlineRetrainer(obj,SignalSource,SignalClassifier,TrainingData);
+            
+            period = 0.15;
+            fprintf('[%s] Setting timer refresh rate to %4.2f s\n',mfilename,period);
+            obj.Timer.Period = period;
             
         end
         function getIntent(obj)
@@ -130,14 +157,14 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
         
         function update(obj)
             % main timer update command
-
+            
             try
                 % get intent
                 getIntent(obj);
                 
                 % update retrainer
                 updateRetrainer(obj);
-
+                
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 % Custom commands here
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -146,6 +173,25 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
                 obj.hTopoState.hHandState.putdata(obj.hTopoState.HandCmd/20);
                 obj.hTopoState.hLegend.setHandCmd(obj.hTopoState.HandCmd);
                 handAngles = obj.hTopoState.hHandState.graspToJointAngles();
+                
+                
+                
+                if obj.ConnectRcp
+                    i = handAngles(action_bus_enum.Index_MCP);
+                    m = handAngles(action_bus_enum.Middle_MCP);
+                    
+                    %                     cmd = sprintf('P1%d',round(i*10));
+                    %                     disp(['Set fingers: ' cmd]);
+                    %                     fprintf(obj.hDevice,string2Can(cmd));
+                    %                     pause(0.02)
+                    
+                    cmd = sprintf('PA%d',round(i*10));
+                    disp(['Set fingers: ' cmd]);
+                    fprintf(obj.hDevice,string2Can(cmd));
+                    
+                    %pause(0.2);
+                end
+                
                 
                 % set wrist angles
                 idx = action_bus_enum.Wrist_FE;
@@ -170,7 +216,7 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
                 obj.hTopoState.hOutput.set_upper_arm_angles_degrees(obj.hTopoState.JointAnglesDegrees);
                 obj.hTopoState.hOutput.set_hand_angles_degrees(handAngles);
                 obj.hTopoState.hOutput.redraw();
-
+                
                 
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 if obj.Verbose > 1, fprintf('\n'); end
@@ -190,16 +236,16 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
             
             p.guiName = 'MiniVIE Grasp Assist';
             p.filePrefix = 'GraspAssist_';
-
+            
             %p.hSource = Inputs.CpchSerial('COM1');
             if mode == 1
                 p.hSource = Inputs.SignalSimulator;
             else
                 p.hSource = Inputs.IntanUdp.getInstance;
-                p.hSource.addfilter(Inputs.Notch([120 180 240 300 360],64,1,1000));
+                %p.hSource.addfilter(Inputs.Notch([120 180 240 300 360],64,1,1000));
                 Fs = p.hSource.SampleFrequency;
-                %p.hSource.addfilter(Inputs.HighPass(10,8,Fs));
-                p.hSource.addfilter(Inputs.LowPass(400,8,Fs));
+                p.hSource.addfilter(Inputs.HighPass(10,8,Fs));
+                %p.hSource.addfilter(Inputs.LowPass(400,8,Fs));
             end
             
             p.ClassNames = {...
@@ -209,10 +255,58 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
                 'Wrist Rotate In' ...
                 'Wrist Rotate Out' ...
                 'No Movement'};
+            %             p.ClassNames = {...
+            %                 'Up' 'Down' 'Left' 'Right' ...
+            %                 'No Movement'};
             
             p.hPresentation = Scenarios.GraspAssist;
+            p.hPresentation.ConnectRcp = true;
             
             obj = foo(p);
+            
+        end
+        function demoRcp
+            % Scenarios.GraspAssist.demoRcp('COM12')
+            %
+            % Simple demo to connect to RCP device, home, and then move to
+            % a range of motion.
+            %
+            % Commands are:
+            %  HA (Home All)
+            %  PA500 (Move all fingers to normalized position of about
+            %  halfway through range of motion (0-1024) )
+            %  PA0 (Move all fingers to zero position
+            % 
+            % Serial commands are run through the serial to CAN which
+            % prepends serial header for usage with canSerial adaptor
+            if nargin < 1
+                comPort = 'COM12';
+            end
+            
+            s = serialSetup(comPort);
+            fprintf('Connecting serial device on %s...\n',comPort)
+            
+            % Give device time to initialize
+            pause(2);
+            
+            % Home all
+            if strcmp('OK',questdlg('Ready to home?','Home All Actuators','OK','Cancel','OK'))
+                disp('Home All');
+                fprintf(s,string2Can('HA'));
+                disp('[Press any key to continue]'),pause
+            end
+            
+            % Close halfway
+            cmd = sprintf('PA%d',500);
+            disp(['Set fingers: ' cmd]);
+            fprintf(s,string2Can(cmd));
+            disp('[Press any key to continue]'),pause
+
+            % Open
+            cmd = sprintf('PA%d',0);
+            disp(['Set fingers: ' cmd]);
+            fprintf(s,string2Can(cmd));
+            disp('[Press any key to continue]'),pause
             
         end
     end
@@ -238,12 +332,12 @@ set(obj.hg.SignalSourceButtons(:),'Enable','on');
 % set(obj.hg.popups(1),'Value',6);
 
 % Setup filters and remaining properties
-obj.println('Adding Filters',1);
-Fs = h.SampleFrequency;
-h.addfilter(Inputs.HighPass(15,3,Fs));
+% obj.println('Adding Filters',1);
+% Fs = h.SampleFrequency;
+% h.addfilter(Inputs.HighPass(15,3,Fs));
 % h.addfilter(Inputs.RemoveOffset(10));
 % h.addfilter(Inputs.Notch([120 240 360],5,1,Fs));
-h.addfilter(Inputs.Notch([120 240 360],64,1,1000));
+% h.addfilter(Inputs.Notch([120 240 360],64,1,1000));
 obj.SignalSource = h;
 
 % f = GUIs.guiSignalViewer(h);
@@ -256,7 +350,7 @@ h = SignalAnalysis.Lda();
 set(obj.hg.SignalAnalysisButtons(:),'Enable','on');
 set(obj.hg.popups(2),'Value',2);
 
-h.NumMajorityVotes = 7;
+h.NumMajorityVotes = 0;
 
 NumSamplesPerWindow = 200;
 fprintf('Setting Window Size to: %d\n',NumSamplesPerWindow);
@@ -264,7 +358,7 @@ h.NumSamplesPerWindow = NumSamplesPerWindow;
 
 %obj.TrainingData = PatternRecognition.TrainingData();
 obj.TrainingData = TrainingDataAnalysis();
-ok = 0;%obj.TrainingData.loadTrainingData([]);
+ok = obj.TrainingData.loadTrainingData([]);
 if ~ok
     obj.TrainingData.initialize(obj.SignalSource.NumChannels,h.NumSamplesPerWindow);
 end
@@ -308,5 +402,52 @@ set(obj.hg.PresentationButtons(:),'Enable','on');
 % set(obj.hg.popups(5),'Value',5);
 
 obj.Presentation = h;
+
+end
+
+function s = serialSetup(strComPort)
+% Setup matlab serial port.  Checks for existing ports first
+% Usage: s = serialSetup('COM9')
+
+if nargin < 1
+    strComPort = 'COM2';
+end
+
+% Check for existing ports
+s = instrfind('Port',strComPort);
+if isempty(s)
+    s = serial(strComPort);
+end
+s.BaudRate = 57600;
+% s.Timeout = 0.1;
+s.Terminator = 'CR';
+
+% Optional: Enable function to echo responses to console
+% set(s, 'BytesAvailableFcn', @(src,evnt){fprintf(2,'[%s:%f]',mfilename,now); fprintf(2,'%c',uint8(fscanf(src))); fprintf('\r')} );
+
+% Open port
+if strcmpi(s.Status,'closed')
+    fopen(s);
+end
+end
+
+function canOut = string2Can(strIn)
+% Convert a text string into CAN compatible message.
+% Format is:
+% T [Extended CAN Addressing Mode]
+% XXXXXXXX [CAN Address in Hex 0-F]
+% Number of Bytes [1-8]
+% Data Bytes in Hex Pairs 00-FF
+%
+% example: string2Can('TEST') produces: 'T1FFFFFFF454455354' in which 4
+% data bytes are indicated, followed by T=0x54 E=0x45 S=0x53 T=0x54
+
+if ~ischar(strIn) || isempty(strIn) || (length(strIn) > 8)
+    error('Expected a string with 1-8 characters\n');
+end
+
+canAddress = '1FFFFFFF';
+hexVals = dec2hex(uint8(strIn));
+canOut = sprintf('T%8s%d%s\n',canAddress,size(hexVals,1),hexVals');
 
 end
