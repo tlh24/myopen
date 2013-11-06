04a7974f87c964c23185110370a2fa7a30ba3476
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2013-11-06 21:50:53 +0000

    Added test function for manual control of Vulcan X

---
+++ MiniVIE/+MPL/TEST_VulcanX.m
@@ -0,0 +1,41 @@
+%% Quick test of vMPL / MPL control via VulcanX
+%  Create the udp transmission via pnet
+UdpLocalPort = 56789;
+UdpDestinationPort = 9027; %9024 = Left; 9027 = Right; (see 
+UdpAddress = '127.0.0.1'; % '192.168.1.101';
+% PnetClass(localPort,remotePort,remoteIP)
+hSink = PnetClass(UdpLocalPort,UdpDestinationPort,UdpAddress);
+hSink.initialize()
+
+% Create the command encoder which translates joint angles to
+% command bytes
+mce = MPL.MudCommandEncoder;
+upperArmAngles = zeros(1,7);
+handAngles = zeros(1,20);
+
+%% Specify joint angles and transmit bytes
+upperArmAngles(1) = 0.5;
+upperArmAngles(4) = 1;
+msg = mce.AllJointsPosVelCmd(upperArmAngles,zeros(1,7),handAngles,zeros(1,20));
+hSink.putData(msg);
+
+%% Specify joint angles and ROC command
+graspId = 4;
+graspValue = 0.8;
+upperArmAngles(1) = 0.5;
+upperArmAngles(3) = 0.5;
+upperArmAngles(4) = 1;
+msg = mce.ArmPosVelHandRocGrasps(upperArmAngles,zeros(1,7),1,graspId,graspValue,1);
+hSink.putData(msg);
+
+%% Specify joint angles using MiniVIE ROC command (user editable)
+localRoc = MPL.RocTable.createRocTables();
+graspId = 2;
+graspValue = 0.8;
+roc = localRoc(graspId+1);
+handAngles = interp1(roc.waypoint,roc.angles,graspValue);
+upperArmAngles(1) = 0.3;
+upperArmAngles(3) = 0.3;
+upperArmAngles(4) = 1.3;
+msg = mce.AllJointsPosVelCmd(upperArmAngles,zeros(1,7),handAngles,zeros(1,20));
+hSink.putData(msg);
