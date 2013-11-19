227d697c99c7e9acf86f8d968216cb9f8ebee548
joseph.odoherty@gmail.com <joseph.odoherty@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2013-11-19 08:17:13 +0000

    * make matstor able to save doubles.   this ci involves a lot of copypasta; can clean later.

--- trunk/common_host/matStor.h
+++ trunk/common_host/matStor.h
@@ -1,23 +1,43 @@
 #ifndef __MATSTOR_H__
 #define __MATSTOR_H__
 
+#include <boost/multi_array.hpp>
+#include <map>
+
 class MatStor{
-	typedef boost::multi_array<float, 3> array3; 
-	typedef boost::multi_array<float, 2> array2; 
-	
 	std::string m_name; //name of the file.
-	std::map<std::string, std::vector<float> > m_dat1; //one dimensional
-	std::map<std::string, array2> m_dat2; //two dimensions
-	std::map<std::string, array3> m_dat3; //three!
+
+	std::map<std::string, std::vector<float> > m_dat1; // 1-d float
+	std::map<std::string, std::vector<double> > m_datd1; // 1-d double
+
+	typedef boost::multi_array<float, 2> array2;
+	typedef boost::multi_array<double, 2> arrayd2; 
+	std::map<std::string, array2> m_dat2; // 2-d float
+	std::map<std::string, arrayd2> m_datd2; // 2-d double
+
+	typedef boost::multi_array<float, 3> array3; 
+	typedef boost::multi_array<double, 3> arrayd3;
+	std::map<std::string, array3> m_dat3; // 3-d float!
+	std::map<std::string, arrayd3> m_datd3; // 3-d double!	
+
+
 public:
 	MatStor(const char* fname); 
 	~MatStor(); 
 	void save(); 
+
 	void setValue(int ch, const char* name, float val);
 	void setValue2(int ch, int un, const char* name, float val);
 	void setValue3(int ch, int un, const char* name, float* val, int siz);
 	float getValue(int ch, const char* name, float def);
 	float getValue2(int ch, int un, const char* name, float def);
 	void getValue3(int ch, int un, const char* name, float* val, int siz);
+
+	void setDouble(int ch, const char* name, double val);
+	void setDouble2(int ch, int un, const char* name, double val);
+	void setDouble3(int ch, int un, const char* name, double* val, int siz);
+	double getDouble(int ch, const char* name, double def);
+	double getDouble2(int ch, int un, const char* name, double def);
+	void getDouble3(int ch, int un, const char* name, double* val, int siz);
 }; 
 #endif
