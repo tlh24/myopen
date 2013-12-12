c0af655bcb32359ab79f37d17c3099a26751053c
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2013-12-12 16:38:45 +0000

    fixed node check error

--- MiniVIE/+Presentation/@MiniV/private/f_KChain_IsectNodes.m
+++ MiniVIE/+Presentation/@MiniV/private/f_KChain_IsectNodes.m
@@ -10,7 +10,14 @@ vB = KChain(iSegment).nodeData; %% link i
 quantum = 0.00001;
 vA = round(vA/quantum)*quantum;
 vB = round(vB/quantum)*quantum;
-[edges,IA,IB] = intersect(vA,vB,'rows');
+
+% RSA: added check for empty nodes since this is an error in R2013b
+if ~isempty(vA) && ~isempty(vA)
+    [edges,IA,IB] = intersect(vA,vB,'rows');
+else
+    IA = [];
+    IB = [];
+end
 
 %% Nodes of the current (i_th) link which intersect with parent
 KChain(iSegment).IsectNodes_1{1} = IB;
