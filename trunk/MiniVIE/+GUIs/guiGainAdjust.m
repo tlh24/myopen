7af81b2d5f1cb869e7f5650f4d2580e62eb422a6
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2014-03-25 18:06:24 +0000

    changed input argument to be just the classifier. stopping timers is left to the outside world

--- trunk/MiniVIE/+GUIs/guiGainAdjust.m
+++ trunk/MiniVIE/+GUIs/guiGainAdjust.m
@@ -1,55 +1,72 @@
-function guiGainAdjust(obj)
-obj.Presentation.stop;
+function guiGainAdjust(SignalClassifier)
+% Open a gui to set the output gain for channels associated with a signal
+% classifer
+%
+% Note there can be issues if this gui is setup while a timer is running.
+% so background processes should be pause until th figure is raised.
+% e.g.
+% obj.Presentation.stop;
+% GUIs.guiGainAdjust(obj.SignalClassifier)
+% obj.Presentation.start;
+%
+% Revisions:
+%   3/25/2014 Armiger: changed input argument to be just the classifier.
+%   stopping timers is left to the outside world
+
 drawnow
 f = UiTools.create_figure('Output Gain','guiClassifierChannels');
 clf(f)
 
 % classNames = GUIs.guiClassifierChannels.getSavedDefaults();
-assert(~isempty(obj.SignalClassifier),'Classifier does not exist');
-classNames = obj.SignalClassifier.getClassNames;
+assert(~isempty(SignalClassifier),'Classifier does not exist');
+classNames = SignalClassifier.getClassNames;
 
-N = length(classNames);
-s = zeros(1,N);
-h = cell(1,N);
-t = zeros(1,N);
-for i = 1:N
-    s(i) = subplot(1,N,i,'Parent',f);
-    set(s(i),'XTick',[]);
-    if i > 1
-        set(s(i),'YTick',[]);
+numClasses = length(classNames);
+s = zeros(1,numClasses);
+h = cell(1,numClasses);
+t = zeros(1,numClasses);
+for iClass = 1:numClasses
+    s(iClass) = subplot(1,numClasses,iClass,'Parent',f);
+    set(s(iClass),'XTick',[]);
+    if iClass > 1
+        set(s(iClass),'YTick',[]);
     end
-    ylim(s(i),[0 10]);
+    ylim(s(iClass),[0 10]);
 
-    t(i) = text('Position',[0.5 5],'String',classNames{i},'Rotation',90,'Parent',s(i));
+    t(iClass) = text('Position',[0.5 5],'String',classNames{iClass},'Rotation',90,'Parent',s(iClass));
     
-    h{i} = GUIs.widgetSlider('Parent',s(i));
-    h{i}.Range = [0 10];
-    h{i}.Type = {'Vertical'};
-    h{i}.PatchWidth = 0.3;
-    h{i}.Value = 0.5;
+    h{iClass} = GUIs.widgetSlider('Parent',s(iClass));
+    h{iClass}.Range = [0 10];
+    h{iClass}.Type = {'Vertical'};
+    h{iClass}.PatchWidth = 0.3;
+    h{iClass}.Value = 0.5;
     
 end
 
-%%
-if isequal(obj.SignalClassifier.OutputChannelGain,1)
+% convert scalar to array of gain values
+if isequal(SignalClassifier.OutputChannelGain,1)
     % Init case
-    obj.SignalClassifier.OutputChannelGain = ones(1,N);
+    SignalClassifier.OutputChannelGain = ones(1,numClasses);
 end
 
-for i = 1:N
-    h{i}.Value = obj.SignalClassifier.OutputChannelGain(i);
+% get the current values from the classifier and set current value
+for iClass = 1:numClasses
+    h{iClass}.Value = SignalClassifier.OutputChannelGain(iClass);
 end
 
-
-for i = 1:N
-%     updateGain = @(src,evt) obj.SignalClassifier.OutputChannelGain(i)
-    h{i}.ButtonUpFcn = @(src,evt)updateGain(i,evt);
+% assign the callback function
+for iClass = 1:numClasses
+    %perform update on mouse move
+    % updateGain = @(src,evt) SignalClassifier.OutputChannelGain(i)
+    %perform update on mouse button release
+    h{iClass}.ButtonUpFcn = @(src,evt)updateGain(SignalClassifier,iClass,evt);
 end
 
-    function updateGain(classId,val)
-        obj.SignalClassifier.OutputChannelGain(classId) = val;
+    % local function to set gain value
+    function updateGain(hClassifier,classId,val)
+        hClassifier.OutputChannelGain(classId) = val;
     end
+
 drawnow
-obj.Presentation.start;
 
 end
