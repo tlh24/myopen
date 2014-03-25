12a22948b98471802cd9cc9f83c46a9ff7234cd8
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2014-03-25 18:53:15 +0000

    moved training buttons and exported pca plot function

--- trunk/MiniVIE/MiniVIE.m
+++ trunk/MiniVIE/MiniVIE.m
@@ -169,6 +169,24 @@ classdef MiniVIE < Common.MiniVieObj
                 'String','Classifier Parameters',...
                 'Enable','off',...
                 'Callback',@(src,evt)obj.pbClassifierProperties);
+            obj.hg.SignalAnalysisButtons(3) = uicontrol(obj.hg.Figure,...
+                'Position',pos('cntrl',MiniVIE.SA,5,1,1),...
+                'Style','pushbutton',...
+                'String','Clear Training Data',...
+                'Enable','off',...
+                'Callback',@(src,evt)obj.pbTrainClear());
+            obj.hg.SignalAnalysisButtons(4) = uicontrol(obj.hg.Figure,...
+                'Position',pos('cntrl',MiniVIE.SA,6,1,1),...
+                'Style','pushbutton',...
+                'String','Train',...
+                'Enable','off',...
+                'Callback',@(src,evt)obj.pbTrain());
+            obj.hg.SignalAnalysisButtons(5) = uicontrol(obj.hg.Figure,...
+                'Position',pos('cntrl',MiniVIE.SA,7,1,1),...
+                'Style','pushbutton',...
+                'String','Plot PCA',...
+                'Enable','off',...
+                'Callback',@(src,evt)obj.pbPlotPca());
             
             % TrainingButtons
             obj.hg.TrainingButtons(1) = uicontrol(obj.hg.Figure,...
@@ -177,24 +195,6 @@ classdef MiniVIE < Common.MiniVieObj
                 'String','Begin Training',...
                 'Enable','off',...
                 'Callback',@(src,evt)obj.pbBeginTraining());
-            obj.hg.TrainingButtons(2) = uicontrol(obj.hg.Figure,...
-                'Position',pos('cntrl',MiniVIE.TRAINING,4,1,1),...
-                'Style','pushbutton',...
-                'String','Clear Training Data',...
-                'Enable','off',...
-                'Callback',@(src,evt)obj.pbTrainClear());
-            obj.hg.TrainingButtons(3) = uicontrol(obj.hg.Figure,...
-                'Position',pos('cntrl',MiniVIE.TRAINING,5,1,1),...
-                'Style','pushbutton',...
-                'String','Train',...
-                'Enable','off',...
-                'Callback',@(src,evt)obj.pbTrain());
-            obj.hg.TrainingButtons(4) = uicontrol(obj.hg.Figure,...
-                'Position',pos('cntrl',MiniVIE.TRAINING,6,1,1),...
-                'Style','pushbutton',...
-                'String','Plot PCA',...
-                'Enable','off',...
-                'Callback',@(src,evt)obj.pbPlotPca());
             
             % PresentationButtons
             obj.hg.PresentationButtons(1) = uicontrol(obj.hg.Figure,...
@@ -895,90 +895,14 @@ classdef MiniVIE < Common.MiniVieObj
         end
         function pbPlotPca(obj)
             % plot the principle components of the current training data
-            
-            
-            hData = obj.TrainingData;
-                        
-            features3D = hData.getFeatureData;
-            s = obj.SignalClassifier;
-            featureColumns = s.convertfeaturedata(features3D);
-            
-            
-            numSamples = size(featureColumns,2);
-            
-            if numSamples == 0
-                errordlg('No Training Data')
-                return
-            end
-            
-            
-            meanFeats = mean(featureColumns,2);        % find mean of each TD feature on a per channel basis
-            stdFeats = std(featureColumns,0,2);        % find standard dev. of TD features
-            minStd = 0.001;                            % Prevents std dev from being zero thus causing div by zero below
-            stdFeats = max(stdFeats,minStd);           % Prevents std dev from being zero thus causing div by zero below
-            
-            normFeats = featureColumns;                         % copy data
-            normFeats = bsxfun(@minus,normFeats,meanFeats);     % subtract mean
-            normFeats = bsxfun(@rdivide,normFeats,stdFeats);    % divide by std dev
-            
-            [Evec,~,~] = svd(normFeats);       % Only compute Eigenvectors using training data
-            allPC = (normFeats'*Evec)';     % full array of PC Training Coefficients
-            
-
-            classLabelId = hData.getClassLabels;
-            uniqueLabels = unique(classLabelId);
-            numClasses = length(uniqueLabels);
-            
-            
-            % Sub-sample PC Inputs:
-            targetNum = 500;
-            subSample = ceil(length(allPC)./targetNum);
-            allPC = allPC(:,1:subSample:end);
-            classLabelId = classLabelId(:,1:subSample:end);
-            
-            f = figure(99);
-            drawnow;
-            clf(f)
-            
-            
-            pcIds = {[1 2] [3 4] [5 6]}; % nPrincipleComponents
-            for i = 1:3%length(pcIds)
-                if i == 3
-                    hAxes = subplot(2,2,[3 4],'Parent',f);
-                else
-                    hAxes = subplot(2,2,i,'Parent',f);
-                end
-                
-                PC = allPC(pcIds{i},:);        % array of PC Training Coefficients trimmed to NPC to keep
-                [X,Y,G] = computePcaContour(PC,classLabelId);
-                
-                % plot result
-                c = distinguishable_colors(length(uniqueLabels));
-                
-                hold(hAxes,'on');
-                hScatter = zeros(1,numClasses);
-                hContour = zeros(1,numClasses);
-                for iClass = 1:numClasses
-                    % create contour outlines
-                    contourLevel = [2 3];
-                    [~, hContour(iClass)] = contour(hAxes,X,Y,G(:,:,iClass),contourLevel);
-                    set(hContour(iClass),'Color',c(iClass,:));
-                
-                    % plot data samples
-                    thisClass = classLabelId == uniqueLabels(iClass);
-                    hScatter(iClass) = plot(hAxes,PC(1,thisClass),PC(2,thisClass),'.');
-                    set(hScatter(iClass),'Color',c(iClass,:));
-                end
-                axis(hAxes,[min(PC(:)) max(PC(:)) min(PC(:)) max(PC(:))]);
-                xlabel(hAxes,''); ylabel(hAxes,''); title(hAxes,'');
-
-            end
-            hLegend = legend(hContour,hData.ClassNames(uniqueLabels));
-            set(hLegend,'Location','NorthEastOutside');
-            
-        end % pbPlotPca
+            GUIs.guiPlotPca(obj.TrainingData)
+        end
         function pbAdjustGains(obj)
-            GUIs.guiGainAdjust(obj);
+            % Launch the gain adjust GUI.  Stop refresh during figure
+            % creation
+            obj.Presentation.stop;
+            GUIs.guiGainAdjust(obj.SignalClassifier)
+            obj.Presentation.start;
         end
         function pbAdjustLimits(obj)
             h = GUIs.guiLimitsAdjust();
