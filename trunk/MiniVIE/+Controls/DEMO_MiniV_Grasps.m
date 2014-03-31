0d4eb8d54716aa13080983b230edf98c0f7d3eb1
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2014-03-31 14:06:38 +0000

    added cyberglove demos and auto cal

--- trunk/MiniVIE/+Controls/DEMO_MiniV_Grasps.m
+++ trunk/MiniVIE/+Controls/DEMO_MiniV_Grasps.m
@@ -12,3 +12,22 @@ handAngles = Controls.graspInterpolation(pctGrasp, id);
 hMiniV.set_hand_angles_degrees(handAngles)
 % a.set_upper_arm_angles_degrees(handAngles)
 hMiniV.redraw 
+
+
+%%
+hCyberGlove = Inputs.CyberGloveSerial('COM11');
+%%
+StartStopForm([])
+while StartStopForm
+    
+    handAngles = getdata(hCyberGlove);
+
+    handAnglesDegrees = handAngles * 180 / pi;
+
+    % set the new angles and redraw
+    hMiniV.set_hand_angles_degrees(handAnglesDegrees)
+    hMiniV.redraw 
+    drawnow
+end
+
+
