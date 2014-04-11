6e1dc71f66be2e1640b2fd32ccfdd33f689eded3
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2014-04-11 19:33:25 +0000

    updated can based compact protocol interface

--- MiniVIE/+Scenarios/GraspAssist.m
+++ MiniVIE/+Scenarios/GraspAssist.m
@@ -33,7 +33,7 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
             
             % setup serial device
             if obj.ConnectRcp
-                comPort = 'COM12';
+                comPort = 'COM26';
                 obj.hDevice = serialSetup(comPort);
                 fprintf('Connecting serial device on %s...\n',comPort)
                 
@@ -265,7 +265,7 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
             obj = foo(p);
             
         end
-        function demoRcp
+        function demoRcp(comPort)
             % Scenarios.GraspAssist.demoRcp('COM12')
             %
             % Simple demo to connect to RCP device, home, and then move to
@@ -276,11 +276,15 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
             %  PA500 (Move all fingers to normalized position of about
             %  halfway through range of motion (0-1024) )
             %  PA0 (Move all fingers to zero position
-            % 
+            %
+            % Compact Conformal End Effector v1 (EZMotion, 1724 Brushed)
+            %    RS485 --> arduino nano --> serial, can headers
+            %    Manual thumb ab/ad
+            %
             % Serial commands are run through the serial to CAN which
             % prepends serial header for usage with canSerial adaptor
             if nargin < 1
-                comPort = 'COM12';
+                comPort = 'COM26';
             end
             
             s = serialSetup(comPort);
@@ -301,12 +305,281 @@ classdef GraspAssist < Scenarios.OnlineRetrainer
             disp(['Set fingers: ' cmd]);
             fprintf(s,string2Can(cmd));
             disp('[Press any key to continue]'),pause
-
+            
             % Open
             cmd = sprintf('PA%d',0);
             disp(['Set fingers: ' cmd]);
             fprintf(s,string2Can(cmd));
             disp('[Press any key to continue]'),pause
+            
+        end
+        function demoRcpJoystick(comPort)
+            % Simple demo to connect to RCP device, home, and then move to
+            % a range of motion.
+            %
+            % Commands are:
+            %  HA (Home All)
+            %  PA500 (Move all fingers to normalized position of about
+            %  halfway through range of motion (0-1024) )
+            %  PA0 (Move all fingers to zero position
+            %
+            % Compact Conformal End Effector v1 (EZMotion, 1724 Brushed)
+            %    RS485 --> arduino nano --> serial, can headers
+            %    Manual thumb ab/ad
+            %
+            % Serial commands are run through the serial to CAN which
+            % prepends serial header for usage with canSerial adaptor
+            
+            if nargin < 1
+                comPort = 'COM26';
+            end
+            
+            try,clear joymex,end
+            JoyMEX('init',0);
+            
+            StartStopForm([]);
+            
+            
+            s = serialSetup(comPort);
+            
+            % Home all
+            if strcmp('OK',questdlg('Ready to home?','Home All Actuators','OK','Cancel','OK'))
+                disp('Home All');
+                fprintf(s,string2Can('HA'));
+                disp('[Press any key to continue]'),pause
+            end
+            
+            i = 0;
+            while StartStopForm
+                
+                i = mod(i,3) + 1;
+                
+                if(i == 1)
+                    fingerID = '1';
+                elseif(i==2)
+                    fingerID = '2';
+                elseif(i==3)
+                    fingerID = 'T';
+                end
+                
+                [a ab] = JoyMEX(0);
+                %force single finger
+                %i=1;[a ab] = JoyMEX(0);
+                if(ab(4))
+                    fprintf(s,string2Can('HA'));
+                elseif(ab(1))
+                    fprintf(s,string2Can('CTA'));
+                elseif(ab(2))
+                    fprintf(s,string2Can('CTB'));
+                elseif(ab(3))
+                    fprintf(s,string2Can('CTC'));
+                else
+                    fingersJoystickAxis = 1; %% depends on joystick
+                    v1 = a(fingersJoystickAxis) * (abs(a(fingersJoystickAxis)) > 0.15);
+                    
+                    fingersJoystickAxis = 3; %% depends on joystick
+                    v3 = a(fingersJoystickAxis) * (abs(a(fingersJoystickAxis)) > 0.15);
+                    
+                    V1 = round(v1*1000);
+                    V3 = round(v3*1000);
+                    %This produces human readable output, e.g. 'V1-1000', but
+                    %MoPro Node doesn't understand this.
+                    %MoPro requires 'V1' [B0] [B1] [B2] [B3]
+                    %Where B0-B3 is the raw binary representation of the
+                    %velocity
+                    fprintf(s,string2Can(sprintf('V%d%d',i,V3)))
+                    %V3Raw = typecast(int32(V3),'uint8');
+                    %fprintf(s,string2Can(sprintf('V%c%c%c%c%c',fingerID,V3Raw(1),V3Raw(2),V3Raw(3),V3Raw(4))));
+                end
+                
+                %Example position command
+                %Pick a position
+                %pos = 200000
+                %Choose motor ID as a CHAR
+                %motorId = '1'
+                %
+                %No changes below
+                %posRaw = typecast(int32(pos),'uint8');
+                %rpcCmd = sprintf('P%c%c%c%c%c',motorId,posRaw(0),posRaw(1), posRaw(2), posRaw(3))));
+                %fprintf(s,string2Can(rpcCmd));
+                pause(0.1);
+                
+            end
+            
+        end
+        function demoRcpCanJoystick(comPort)
+            % Simple demo to connect to RCP device, home, and then move to
+            % a range of motion.
+            %
+            % Commands are:
+            %  HA (Home All)
+            %  PA500 (Move all fingers to normalized position of about
+            %  halfway through range of motion (0-1024) )
+            %  PA0 (Move all fingers to zero position
+            %
+            % Compact Conformal End Effector v1 (EZMotion, 1724 Brushed)
+            %    RS485 --> arduino nano --> serial, can headers
+            %    Manual thumb ab/ad
+            %
+            % Serial commands are run through the serial to CAN which
+            % prepends serial header for usage with canSerial adaptor
+            
+            persistent lastCommand
+            
+            if nargin < 1
+                comPort = 'COM26';
+            end
+            
+            if isempty(lastCommand)
+                lastCommand = 0;
+            end
+            
+            try,clear joymex,end
+            JoyMEX('init',0);
+            
+            StartStopForm([]);
+            
+            
+            % Setup serial port
+            s = serialSetup(comPort);
+            s.Timeout = 0.2;
+            
+            % Perform Serial-CAN initialization
+            canSpeed = 8;
+            % 4/10/2014
+            % Initialize can message wrapper
+            Scenarios.GraspAssist.initializeSerialCanConverter(s,canSpeed)
+            
+            % Home all
+            if strcmp('OK',questdlg('Ready to home?','Home All Actuators','OK','Cancel','OK'))
+                disp('Home All');
+                fprintf(s,string2Can('HA'));
+                disp('[Press any key to continue]'),pause
+            end
+            
+            while StartStopForm
+                
+                % get joystick input
+                [a, ab] = JoyMEX(0);
+                
+                if(ab(4))
+                    msg = string2Can('HA');
+                    fprintf(s,msg);                 % send msg to comm port
+                    fprintf(sprintf('%s\n',msg));   % echo msg to console
+                elseif(ab(1))
+                    msg = string2Can('CTA');
+                    fprintf(s,msg);                 % send msg to comm port
+                    fprintf(sprintf('%s\n',msg));   % echo msg to console
+                elseif(ab(2))
+                    msg = string2Can('CTB');
+                    fprintf(s,msg);                 % send msg to comm port
+                    fprintf(sprintf('%s\n',msg));   % echo msg to console
+                elseif(ab(3))
+                    fprintf(1,'Command CTC Disabled\n');
+                    msg = '';
+                    %fprintf(s,string2Can('CTC'));
+                elseif(ab(5))
+                    V3Raw = typecast(int32(300),'uint8');
+                    msg = string2Can(sprintf('V%c%c%c%c%c',...
+                        '1',V3Raw(1),V3Raw(2),V3Raw(3),V3Raw(4)));
+                    fprintf(s,msg);                 % send msg to comm port
+                    fprintf(sprintf('%s\n',msg));   % echo msg to console
+                elseif(ab(6))
+                    V3Raw = typecast(int32(300),'uint8');
+                    msg = string2Can(sprintf('V%c%c%c%c%c',...
+                        '2',V3Raw(1),V3Raw(2),V3Raw(3),V3Raw(4)));
+                    fprintf(s,msg);                 % send msg to comm port
+                    fprintf(sprintf('%s\n',msg));   % echo msg to console
+                elseif(ab(7))
+                    V3Raw = typecast(int32(300),'uint8');
+                    msg = string2Can(sprintf('V%c%c%c%c%c',...
+                        'T',V3Raw(1),V3Raw(2),V3Raw(3),V3Raw(4)));
+                    fprintf(s,msg);                 % send msg to comm port
+                    fprintf(sprintf('%s\n',msg));   % echo msg to console
+                elseif(ab(8))
+                    V3Raw = typecast(int32(0),'uint8');
+                    fingerID = {'1' '2' 'T'};
+                    
+                    for iFinger = 1:length(fingerID)
+                        msg = string2Can(sprintf('V%c%c%c%c%c',...
+                            fingerID{iFinger},V3Raw(1),V3Raw(2),V3Raw(3),V3Raw(4)));
+                        fprintf(s,msg);                 % send msg to comm port
+                        fprintf(sprintf('%s\n',msg));   % echo msg to console
+                    end
+                else
+                    fingersJoystickAxis = 1; %% depends on joystick
+                    v1 = a(fingersJoystickAxis) * (abs(a(fingersJoystickAxis)) > 0.15);
+                    
+                    fingersJoystickAxis = 3; %% depends on joystick
+                    v3 = a(fingersJoystickAxis) * (abs(a(fingersJoystickAxis)) > 0.15);
+                    
+                    V1 = round(v1*1000);
+                    V3 = round(v3*1000);
+                    %This produces human readable output, e.g. 'V1-1000', but
+                    %MoPro Node doesn't understand this.
+                    %MoPro requires 'V1' [B0] [B1] [B2] [B3]
+                    %Where B0-B3 is the raw binary representation of the
+                    %velocity
+                    %fprintf(s,string2Can(sprintf('V%d%d',i,V3)))
+                    V3Raw = typecast(int32(V3),'uint8');
+                    
+                    if ~isequal(lastCommand,V3)
+                        
+                        fingerID = {'1' '2' 'T'};
+                        
+                        for iFinger = 1:length(fingerID)
+                            msg = string2Can(sprintf('V%c%c%c%c%c',...
+                                fingerID{iFinger},V3Raw(1),V3Raw(2),V3Raw(3),V3Raw(4)));
+                            fprintf(s,msg);                 % send msg to comm port
+                            fprintf(sprintf('%s\n',msg));   % echo msg to console
+                        end
+                        lastCommand = V3;
+                    end
+                end
+                
+                % Read down serial buffer
+                bytesAvailable = s.BytesAvailable;
+                if bytesAvailable
+                    x = fread(s,bytesAvailable);
+                    fprintf('Reply: "%s"\n',char(x));
+                end
+                
+                
+                
+                pause(0.05);
+                
+            end
+            
+        end
+        function initializeSerialCanConverter(hPort,canSpeed)
+            
+            % Always start each session (when your program starts) with sending 2-3 [CR] to empty any prior
+            % comand or queued character in the CAN232 (many times are power up there could be false
+            % characters in the queue or old ones that was from a previous session),
+            fprintf(hPort,sprintf('\n\n\n'));
+            
+            %then check the CAN version with V command (to be sure that you have communication with the unit at correct speed)
+            fprintf(hPort,'V');
+            
+            % then set up the CAN speed with s or S command,
+            speed = sprintf('S%d',canSpeed);
+            fprintf(hPort,speed);
+            
+            %then open the CAN port with O,
+            fprintf(hPort,'O');
+            
+            %then the
+            % CAN232 is in operation for both sending and receiving CAN frames. Send frames with the t ot T
+            % command and wait for a response back to see it it was placed in the CAN FIFO transmission
+            % queue or the queue was full. Incomming frames from the CAN bus will be sent out at ones on the
+            % RS232 or queued in the FIFO if the RS232 is full. Then once in a while send the F command to see
+            % if there are any errors (e.g. each 100-500mS or if you get an error back from the CAN232). If you
+            % get too many errors back after sending commands to the unit, send 2-3 [CR] to empty the buffer,
+            % then issue the commands again, if this continue allert the user or application within your program
+            % that there is a communication error (e.g. a damaged RS232 tranceiver or power failure).
+            
+            %check for errors
+            fprintf(hPort,'F');
             
         end
     end
@@ -410,7 +683,7 @@ function s = serialSetup(strComPort)
 % Usage: s = serialSetup('COM9')
 
 if nargin < 1
-    strComPort = 'COM2';
+    strComPort = 'COM26';
 end
 
 % Check for existing ports
@@ -446,7 +719,7 @@ if ~ischar(strIn) || isempty(strIn) || (length(strIn) > 8)
     error('Expected a string with 1-8 characters\n');
 end
 
-canAddress = '1FFFFFFF';
+canAddress = '028C901A';
 hexVals = dec2hex(uint8(strIn));
 canOut = sprintf('T%8s%d%s\n',canAddress,size(hexVals,1),hexVals');
 
