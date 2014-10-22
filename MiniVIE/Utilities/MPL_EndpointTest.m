4939398c7f2620982175fcfcbf2b318174c8685e
rarmiger@gmail.com <rarmiger@gmail.com@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2014-10-22 04:49:42 +0000

    removed ref to MPLScenarioMud

--- MiniVIE/Utilities/MPL_EndpointTest.m
+++ MiniVIE/Utilities/MPL_EndpointTest.m
@@ -1,11 +1,26 @@
 % Solves the Flock of Birds portion of HW3
 
+%% Quick test of vMPL / MPL control via VulcanX
+%  Create the udp transmission via pnet
+UdpLocalPort = 56789;
+UdpDestinationPort = 9027; %9024 = Left; 9027 = Right; (see
+UdpAddress = '127.0.0.1'; % '192.168.1.101';
+% PnetClass(localPort,remotePort,remoteIP)
+hSink = PnetClass(UdpLocalPort,UdpDestinationPort,UdpAddress);
+hSink.initialize()
+
+% Create the command encoder which translates joint angles to
+% command bytes
+mce = MPL.MudCommandEncoder;
+
+handAngles = zeros(20,1);
+%%
 % Setup MiniVIE Utilities
 MiniVIE.configurePath
 
 T_Arm = makehgtform('translate',[0 -3 8],'yrotate',pi/2,'xrotate',pi/2);
 
-%% Plot the individual frames
+% Plot the individual frames
 
 % create all frames and show stick arm with cyninders
 clf
@@ -43,6 +58,12 @@ axis([-4     8    -8     8    -2    12])
 rotate3d
 grid on
 
+[T, A] = MPL_Frames([0 pi/2 pi/2 pi/2 0]);
+
+for i = 1:size(A,3)
+    set(h(i),'Matrix',A(:,:,i));
+end
+
 %%
 try,hJoystick = JoyMexClass();catch;hJoystick =[];end
 %%
@@ -55,36 +76,54 @@ StartStopForm([]);
 while StartStopForm
     pause(0.05)
     
-
-    hJoystick.getdata();
     
-    joyVal = hJoystick.axisVal(1:3);
-    joyVal(abs(joyVal) < 0.1) = 0;
+    %     hJoystick.getdata();
+    %     joyVal = hJoystick.axisVal(1:3);
+    %     joyVal(abs(joyVal) < 0.1) = 0;
+    %
+    %     endPtVelocities = joyVal(:)/10;
+    endPtVelocities = [-.1 .1 -.1];  % problem with enabled joint limits
+%     endPtVelocities = [-.1 -.1 .1];
     
-    endPtVelocities = joyVal(:)/10;
-%     endPtVelocities = [-.1 0 0];
+    [jV, ~, theta] = MPL_JacobianBound(theta, endPtVelocities);
+    jV
     
-    [jV, J, theta] = MPL_JacobianBound(theta, endPtVelocities);
-    
-    
-%     %%
-%     theta(:) = 0
-%     theta(2) = deg2rad(90)
-%     theta(3) = deg2rad(90)
-%     theta(4) = deg2rad(90)
+    %     %%
+    %     theta(:) = 0
+    %     theta(2) = deg2rad(90)
+    %     theta(3) = deg2rad(90)
+    %     theta(4) = deg2rad(90)
     
     [T, A] = MPL_Frames(theta);
-    for i = 1:size(A,3)
-        set(h(i),'Matrix',A(:,:,i));
-    end
-    %
-    T(1:3,4,end)';
-    %
-    upperArmAngles = [theta(1) theta(2)-pi/2 -(theta(3)-pi/2) theta(4) theta(5)-pi/2 0 0];
-    msg = mce.AllJointsPosVelCmd(upperArmAngles,zeros(1,7),handAngles,zeros(1,20));
-    hSink.putData(msg);
+    for i = 1:size(A,3),set(h(i),'Matrix',A(:,:,i));end
+    upperArmAngles = [theta(1) theta(2)-pi/2 -(theta(3)-pi/2) theta(4) -(theta(5)-pi/2) 0 0];
     
-    F = A(:,:,2)*A(:,:,3)*A(:,:,4)*A(:,:,5);
-    norm(F(1:3,4));
+    hSink.putData(mce.AllJointsPosVelCmd(upperArmAngles,zeros(1,7),handAngles,zeros(1,20)));
+
+    %
+    T(1:3,4,end)'
+    %
+    
 end
+
+return
+%%
+
+% array of joint angles for arm (adjustable)
+offset = [0 pi/2 pi/2 0 pi/2];
+theta = [0 0 0 1 0 ] + offset;
+[T, A] = MPL_Frames(theta);
+
+for i = 1:size(A,3)
+    set(h(i),'Matrix',A(:,:,i));
+end
+%
+T(1:3,4,end)';
+%
+upperArmAngles = [theta(1) theta(2)-pi/2 -(theta(3)-pi/2) theta(4) -(theta(5)-pi/2) 0 0];
+msg = mce.AllJointsPosVelCmd(upperArmAngles,zeros(1,7),handAngles,zeros(1,20));
+hSink.putData(msg);
+
+
+
 
