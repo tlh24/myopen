f04b50036adf7596012d0c58eda3155dc500936e
rarmiger <rarmiger@3b475311-9a41-0410-bf7d-89c94ee2dd84>
2015-02-01 18:37:31 +0000

    improved compatability

--- trunk/MiniVIE/+GUIs/guiSignalViewerState.m
+++ trunk/MiniVIE/+GUIs/guiSignalViewerState.m
@@ -1,7 +1,15 @@
+% classdef guiSignalViewerState
+%    enumeration
+%       FFT
+%       TimeDomain
+%       Features
+%    end   
+% end
+% Updated to remove 'enumeration' for backward compatability
 classdef guiSignalViewerState
-   enumeration
-      FFT
-      TimeDomain
-      Features
+   properties (Constant = true)
+      FFT = 'FFT';
+      TimeDomain = 'TimeDomain';
+      Features = 'Features';
    end   
 end
