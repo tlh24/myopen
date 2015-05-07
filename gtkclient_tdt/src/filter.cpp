148886fdbfa50377d9082828cfc95565ae1840da
Joseph O'Doherty <joseph.odoherty@gmail.com>
2015-05-06 18:52:36 -0700

    fill in missing filters at 24 and 48 khz

--- gtkclient_tdt/src/filter.cpp
+++ gtkclient_tdt/src/filter.cpp
@@ -44,7 +44,7 @@ void Filter::Proc(float *in, float *out, unsigned int kpoints)
 
 
 // 4th-order Butterworth bandpass
-// 300 Hz to 5 kHz @ 24.414 kHz
+// 300 Hz to 5 kHz @ 24.4140625 kHz
 // Create with MATLAB like so:
 /*
 n = 4; % filter order
@@ -81,33 +81,41 @@ FilterButterBand_24k_300_5000::~FilterButterBand_24k_300_5000()
 
 
 
-// 2nd-order Butterworth lowpass
-// 5 kHz @ 24.414 kHz
+// 4th-order Butterworth bandpass
+// 300 Hz to 5 kHz @ 48.828125 kHz
 // Create with MATLAB like so:
 /*
-n = 2; % filter order
-sr = 24414.0625;
+n = 4; % filter order
+sr = 48828.1250;
 hsr = sr/2; % half sample rate
-Wn = [5000/hsr];
-[B,A] = butter(n,Wn);
+Wn = [300/hsr 5000/hsr];
+[B,A] = butter(n/2,Wn);
 sprintf('B.push_back(%0.15e);\n',B)
 sprintf('A.push_back(%0.15e);\n',A)
 sprintf('d.push_back(%d);\n', zeros(n,1))
 */
-FilterButterLow_24k_5000::FilterButterLow_24k_5000()
+FilterButterBand_48k_300_5000::FilterButterBand_48k_300_5000()
 {
-	B.push_back(2.143823578956666e-01);
-	B.push_back(4.287647157913333e-01);
-	B.push_back(2.143823578956666e-01);
+	B.push_back(6.325743528801947e-02);
+	B.push_back(0.000000000000000e+00);
+	B.push_back(-1.265148705760389e-01);
+	B.push_back(0.000000000000000e+00);
+	B.push_back(6.325743528801947e-02);
+
 
 	A.push_back(1.000000000000000e+00);
-	A.push_back(-3.338106676530923e-01);
-	A.push_back(1.913400992357589e-01);
+	A.push_back(-3.132874105110674e+00);
+	A.push_back(3.707314974017143e+00);
+	A.push_back(-2.000492567522976e+00);
+	A.push_back(4.264766788841259e-01);
 
+
+	d.push_back(0);
+	d.push_back(0);
 	d.push_back(0);
 	d.push_back(0);
 }
-FilterButterLow_24k_5000::~FilterButterLow_24k_5000()
+FilterButterBand_48k_300_5000::~FilterButterBand_48k_300_5000()
 {
 }
 
@@ -146,6 +154,38 @@ FilterButterLow_24k_3000::~FilterButterLow_24k_3000()
 
 
 // 2nd-order Butterworth lowpass
+// 5 kHz @ 24.414 kHz
+// Create with MATLAB like so:
+/*
+n = 2; % filter order
+sr = 24414.0625;
+hsr = sr/2; % half sample rate
+Wn = [5000/hsr];
+[B,A] = butter(n,Wn);
+sprintf('B.push_back(%0.15e);\n',B)
+sprintf('A.push_back(%0.15e);\n',A)
+sprintf('d.push_back(%d);\n', zeros(n,1))
+*/
+FilterButterLow_24k_5000::FilterButterLow_24k_5000()
+{
+	B.push_back(2.143823578956666e-01);
+	B.push_back(4.287647157913333e-01);
+	B.push_back(2.143823578956666e-01);
+
+	A.push_back(1.000000000000000e+00);
+	A.push_back(-3.338106676530923e-01);
+	A.push_back(1.913400992357589e-01);
+
+	d.push_back(0);
+	d.push_back(0);
+}
+FilterButterLow_24k_5000::~FilterButterLow_24k_5000()
+{
+}
+
+
+
+// 2nd-order Butterworth lowpass
 // 3 kHz @ 48.8281250 kHz
 // Create with MATLAB like so:
 /*
@@ -172,6 +212,39 @@ FilterButterLow_48k_3000::FilterButterLow_48k_3000()
 	d.push_back(0);
 }
 FilterButterLow_48k_3000::~FilterButterLow_48k_3000()
+{
+}
+
+
+
+
+// 2nd-order Butterworth lowpass
+// 5 kHz @ 48.8281250 kHz
+// Create with MATLAB like so:
+/*
+n = 2; % filter order
+sr = 48828.1250;
+hsr = sr/2; % half sample rate
+Wn = [5000/hsr];
+[B,A] = butter(n,Wn);
+sprintf('B.push_back(%0.15e);\n',B)
+sprintf('A.push_back(%0.15e);\n',A)
+sprintf('d.push_back(%d);\n', zeros(n,1))
+*/
+FilterButterLow_48k_5000::FilterButterLow_48k_5000()
+{
+	B.push_back(7.019288884370953e-02);
+	B.push_back(1.403857776874191e-01);
+	B.push_back(7.019288884370953e-02);
+
+	A.push_back(1.000000000000000e+00);
+	A.push_back(-1.123519836942896e+00);
+	A.push_back(4.042913923177339e-01);
+
+	d.push_back(0);
+	d.push_back(0);
+}
+FilterButterLow_48k_5000::~FilterButterLow_48k_5000()
 {
 }
 
@@ -204,5 +277,39 @@ FilterButterHigh_24k_500::FilterButterHigh_24k_500()
 	d.push_back(0);
 }
 FilterButterHigh_24k_500::~FilterButterHigh_24k_500()
+{
+}
+
+
+
+// 2nd-order Butterworth highpass
+// 500 Hz @ 48.828125 kHz
+// Create with MATLAB like so:
+/*
+n = 2; % filter order
+sr = 48828.1250;
+hsr = sr/2; % half sample rate
+Wn = [500/hsr];
+[B,A] = butter(n,Wn,'high');
+sprintf('B.push_back(%0.15e);\n',B)
+sprintf('A.push_back(%0.15e);\n',A)
+sprintf('d.push_back(%d);\n', zeros(n,1))
+*/
+FilterButterHigh_48k_500::FilterButterHigh_48k_500()
+{
+	B.push_back(9.555237705303598e-01);
+	B.push_back(-1.911047541060720e+00);
+	B.push_back(9.555237705303598e-01);
+
+
+	A.push_back(1.000000000000000e+00);
+	A.push_back(-1.909068426849618e+00);
+	A.push_back(9.130266552718208e-01);
+
+
+	d.push_back(0);
+	d.push_back(0);
+}
+FilterButterHigh_48k_500::~FilterButterHigh_48k_500()
 {
 }
