99912f9c3c845392e101908baafd97946117b763
Joseph O'Doherty <joseph.odoherty@gmail.com>
2016-01-06 20:17:02 -0800

    eliminate some codes

--- gtkclient_tdt/include/gtkclient.h
+++ gtkclient_tdt/include/gtkclient.h
@@ -39,18 +39,11 @@ enum MODES { // ui tabs
 
 #define RECCHAN 	96
 
-//#define LMSBUF 		64
-
 //globals.  could make a class for these but .. eh
 // XXX why NSAMP*3 ???
 extern float	g_fbuf[NFBUF][NSAMP*3]; //continuous waveform. range [-1 .. 1]. For drawing.
 extern i64		g_fbufW; //where to write to (always increment)
 extern i64		g_fbufR; //display thread reads from here - copies to mem
-//extern u32 		g_nsamp; //given the current level of zoom (1 = 4096 samples), how many samples to update?
-
-extern bool 	g_die;
-//extern int 		g_channel[4];
-//extern double	g_minISI;
 
 typedef struct PO8Data {
 	int64_t tick;	// this is the tick for the first sample
