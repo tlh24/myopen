8232a60536b5c0e8f7b65e78a225c3bc1bc81a5d
Joseph O'Doherty <joseph.odoherty@gmail.com>
2016-03-30 19:32:01 -0700

    tweaks to rls, but it's gonna be deleted now

--- gtkclient_tdt/include/rls.h
+++ gtkclient_tdt/include/rls.h
@@ -1,28 +1,35 @@
 #ifndef __RLS_H__
 #define	__RLS_H__
 
+#include <vector>
+#include <atomic>
+#include <mutex>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_blas.h>
+
+class MatStor;
 
 class RLS
 {
 protected:
-	int n; 			// order of the filter
+	size_t n; 		// order of the filter
 	double lambda;	// forgetting factor (try: 1-1/(10*n) < lambda < 1)
 	double laminv;	// lambda^-1, for convenience
 	double delta;	// initial phi-matrix factor (a small number)
-
-	gsl_matrix *w;	// weights
-	gsl_matrix *p;	// autocorrelation matrix of weights
-
+	gsl_matrix *W;	// weights
+	gsl_matrix *Wshadow; 	// shadow weights (for thread safety)
+	std::vector<gsl_matrix *> P; // tensor of matrices of autocorrelations
+	std::mutex m1;			// for thread safety
+	std::mutex m2;			// for thread safety
 public:
 
-	RLS(int _n, double _lambda, double _delta);
+	RLS(int _n, double _lambda, double _delta, MatStor *ms);
 	~RLS();
 
-	void train(gsl_matrix *x, double d);
-
-	double filter(gsl_matrix *x);
+	void train(gsl_matrix *X);
+	void filter(gsl_matrix *X, gsl_matrix *Y);
+	void clearWeights();
+	void save(MatStor *ms);
 };
 
 #endif
--- gtkclient_tdt/src/rls.cpp
+++ gtkclient_tdt/src/rls.cpp
@@ -1,90 +1,195 @@
+#include <vector>
+#include <atomic>
+#include <mutex>
+#include <gsl/gsl_permutation.h>
+#include <gsl/gsl_linalg.h>
 #include <gsl/gsl_blas.h>               // for gsl_blas_dgemm
 #include <gsl/gsl_cblas.h>
 #include <gsl/gsl_matrix_double.h>      // for gsl_matrix_alloc, etc
+#include "matStor.h"                    // for MatStor
 #include "rls.h"                        // for RLS
 
-RLS::RLS(int _n, double _lambda, double _delta)
+RLS::RLS(int _n, double _lambda, double _delta, MatStor *ms)
 {
 	n = _n;
 	lambda = _lambda;
-	laminv = 1.0/lambda;
 	delta = _delta;
 
-	w = gsl_matrix_alloc(n, 1);
-	gsl_matrix_set_all(w, 1.0/(double)n);	// init weights
+	W = gsl_matrix_alloc(n, n);
+	Wshadow = gsl_matrix_alloc(n, n);
+	gsl_matrix_set_all(W, 1.0/(double)n);	// init weights
+	for (size_t i=0; i<n; i++) {
+		gsl_matrix_set(W, i, i, 0.0); 		// diagonal should be zero
+		gsl_matrix *M = gsl_matrix_alloc(n, n);
+		gsl_matrix_set_identity(M); // identity matrix
+		gsl_matrix_scale(M, delta); // scale by delta
+		P.push_back(M);
+	}
 
-	p = gsl_matrix_alloc(n, n);
-	gsl_matrix_set_identity(p);
-	gsl_matrix_scale (p, delta); // scale by delta
+	if (ms) {
+		for (size_t i=0; i<n; i++) {
+			for (size_t j=0; j<n; j++) {
+				double d = ms->getDouble2(i, j, "rls_w", gsl_matrix_get(W, i, j));
+				gsl_matrix_set(W, i, j, d);
+			}
+		}
+		lambda = ms->getDouble(0, "rls_lambda", lambda);
+		laminv = 1.0/lambda;
+		delta = ms->getDouble(0, "rls_delta", delta);
+	}
 }
 
 RLS::~RLS()
 {
-	gsl_matrix_free(w);
-	gsl_matrix_free(p);
+	gsl_matrix_free(W);
+	gsl_matrix_free(Wshadow);
+	for (auto & M : P) {
+		gsl_matrix_free(M);
+	}
 }
 
-// x is the input vector (nx1)
-// d is the desired output scalar
-void RLS::train(gsl_matrix *x, double d)
+// X is the input matrix (n by t)
+void RLS::train(gsl_matrix *X)
 {
 	// in the below we use (roughly) the notation of Haykin, 4th ed. pg 443
 
-	// pi(k) = p(k-1) * x(k)
-	gsl_matrix *pi = gsl_matrix_alloc(n,1);
-	gsl_matrix_set_zero(pi);
-	gsl_blas_dgemm(CblasNoTrans,CblasNoTrans,1.0,p,x,0.0,pi);
+	size_t t = X->size2;
 
-	// tmp(k) = x'(k) * pi(k) + lambda
-	gsl_matrix *tmp = gsl_matrix_alloc(1,1);
-	gsl_matrix_set_all(tmp, 1.0);
-	gsl_blas_dgemm(CblasTrans,CblasNoTrans,1.0,x,pi,lambda,tmp);
+	// copy X -> Y
+	gsl_matrix *Y;
+	Y = gsl_matrix_alloc(n, t);
+	gsl_matrix_memcpy(Y, X);
 
-	// tmpinv(k) = inv(tmp(k))
-	// todo check for div by zero etc xxx
-	gsl_matrix *tmpinv = gsl_matrix_alloc(1,1);
-	gsl_matrix_set_all(tmpinv, 1.0/ gsl_matrix_get(tmp,0,0));
+	gsl_matrix *PI = gsl_matrix_alloc(n,t); 	// intermediate matrix
+	gsl_matrix *TMP = gsl_matrix_alloc(t,t); 	// intermediate matrix
+	gsl_matrix *WORK = gsl_matrix_alloc(t,t); 	// for SVD
+	gsl_matrix *V = gsl_matrix_alloc(t,t);		// for SVD
+	gsl_vector *s = gsl_vector_alloc(t);		// for SVD
+	gsl_matrix *S = gsl_matrix_alloc(t,t);		// for SVD
+	gsl_vector *work = gsl_vector_alloc(t);		// for SVD
+	gsl_matrix *TINV = gsl_matrix_alloc(t,t);	// TMP inverse
+	gsl_matrix *TM2 = gsl_matrix_alloc(t,n); 	// intermediate matrix
+	gsl_matrix *K = gsl_matrix_alloc(n,t);		// gain
+	gsl_matrix *alpha = gsl_matrix_alloc(1,t);	// prior estimation error
 
-	// g(k) = pi(k) * tmpinv(k)
-	// nb Haykin uses k, rather than g
-	gsl_matrix *g = gsl_matrix_alloc(n,1);
-	gsl_matrix_set_zero(g);
-	gsl_blas_dgemm(CblasNoTrans,CblasNoTrans,1.0,pi,tmpinv,0.0,g);
+	gsl_matrix_set_identity(S);
 
-	// alpha(k) = d(k) - w'(k-1) * x(k);
-	// nb Haykin uses xi, rather than alpha
-	gsl_matrix *alpha = gsl_matrix_alloc(1,1);
-	gsl_matrix_set_all(alpha, d);
-	gsl_blas_dgemm(CblasTrans,CblasNoTrans,-1.0,w,x,1.0,alpha);
+	{
+		std::lock_guard<std::mutex> lock(m2);
+		gsl_matrix_memcpy(Wshadow, W);	// W -> Wshadow
+	}
 
-	// w(k) = w(k-1) + g(k) * alpha'(k)
-	gsl_blas_dgemm(CblasNoTrans,CblasTrans,1.0,g,alpha,1.0,w);
+	for (size_t i=0; i<n; i++) {
 
-	// tmp2 = x'(k)*p(k-1)
-	gsl_matrix *tmp2 = gsl_matrix_alloc(1,n);
-	gsl_blas_dgemm(CblasTrans,CblasNoTrans,1.0,x,p,0.0,tmp2);
+		// refresh X (Y -> X)
+		gsl_matrix_memcpy(X, Y);
 
-	// p(k) = laminv * p(k-1) - laminv * g(k) * tmp2
-	gsl_blas_dgemm(CblasNoTrans,CblasNoTrans,-1.0*laminv,g,tmp2,laminv,p);
+		// get w [n by 1]
+		gsl_matrix_view w = gsl_matrix_submatrix(Wshadow, 0, i, n, 1);
+
+		// zero out row in X
+		// prefill alpha with desired values
+		for (size_t k=0; k<t; k++) {
+			gsl_matrix_set(alpha, 0, k, gsl_matrix_get(X, i, k));
+			gsl_matrix_set(X, i, k, 0.0);
+		}
+
+		// PI = P X
+		gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, P[i], X, 0.0, PI);
+
+		// TMP = X' PI + lambda
+		gsl_matrix_set_all(TMP, lambda);
+		gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, X, PI, 1.0, TMP);
+
+		// TMP = U S V^T
+		// n.b. U -> TMP
+		gsl_linalg_SV_decomp_mod(TMP, WORK, V, s, work);
+		for (size_t k=0; k<t; k++) {
+			gsl_matrix_set(S, k, k, gsl_vector_get(s, k));
+		}
+
+		// WORK = V S'
+		gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, V, S, 0.0, WORK);
+
+		// TINV = WORK U'
+		gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, WORK, TMP, 0.0, TINV);
+
+		// K = PI TINV
+		gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, PI, TINV, 0.0, K);
+
+		// alpha = d - w' X
+		// nb Haykin uses xi, rather than alpha
+		gsl_blas_dgemm(CblasTrans, CblasNoTrans, -1.0, &w.matrix, X, 1.0, alpha);
+
+		// w = w + K alpha'
+		gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, K, alpha, 1.0, &w.matrix);
+
+		// TM2 = X' P
+		gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, X, P[i], 0.0, TM2);
+
+		// P = laminv * P - laminv * K * TM2
+		gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, -1.0*laminv, K, TM2, laminv, P[i]);
+
+	}
 
 	// deallocate
-	gsl_matrix_free(pi);
-	gsl_matrix_free(tmp);
-	gsl_matrix_free(tmpinv);
-	gsl_matrix_free(g);
+	gsl_matrix_free(PI);
+	gsl_matrix_free(TMP);
+	gsl_matrix_free(WORK);
+	gsl_matrix_free(V);
+	gsl_vector_free(s);
+	gsl_matrix_free(S);
+	gsl_vector_free(work);
+	gsl_matrix_free(TINV);
+	gsl_matrix_free(TM2);
+	gsl_matrix_free(K);
 	gsl_matrix_free(alpha);
-	gsl_matrix_free(tmp2);
+
+	{
+		std::lock_guard<std::mutex> lock(m1);
+		gsl_matrix_memcpy(W, Wshadow);
+	}
 
 	// todo add bulletproofing for elements in p becoming huge numbers
 	// or becoming nan
 }
 
-// x is the input vector (n,1)
-// output is the filtered value (scalar)
-double RLS::filter(gsl_matrix *x)
+// X is the input matrix (n by t)
+// Y is the cleaned output matrix (n x t)
+void RLS::filter(gsl_matrix *X, gsl_matrix *Y)
 {
-	// dhat(k) = w'(k) * x(k);
-	gsl_matrix *dhat = gsl_matrix_alloc(1,1);
-	gsl_blas_dgemm(CblasTrans,CblasNoTrans,1.0,w,x,0.0,dhat);
-	return gsl_matrix_get(dhat,0,0);
+	// copy X into Y
+	gsl_matrix_memcpy(Y, X);
+	{
+		std::lock_guard<std::mutex> lock(m1);
+		// Y = W * X - X;
+		gsl_blas_dgemm (CblasNoTrans, CblasNoTrans, 1.0, W, X, -1.0, Y);
+	}
+}
+
+void RLS::clearWeights()
+{
+	std::lock_guard<std::mutex> lock1(m1);
+	std::lock_guard<std::mutex> lock2(m2);
+	gsl_matrix_set_all(Wshadow, 1.0/(double)n);	// init weights
+	for (size_t i=0; i<n; i++) {
+		gsl_matrix_set(Wshadow, i, i, 0.0); 	// diagonal should be zero
+		gsl_matrix_set_identity(P[i]); // identity matrix
+		gsl_matrix_scale(P[i], delta); // scale by delta
+	}
+	gsl_matrix_memcpy(W, Wshadow);
+}
+
+void RLS::save(MatStor *ms)
+{
+	if (ms) {
+		std::lock_guard<std::mutex> lock(m1);
+		for (size_t i=0; i<n; i++) {
+			for (size_t j=0; j<n; j++) {
+				ms->setDouble2(i, j, "rls_w", gsl_matrix_get(W,i,j));
+			}
+		}
+		ms->setDouble(0, "rls_lambda", lambda);
+		ms->setDouble(0, "rls_delta", delta);
+	}
 }
