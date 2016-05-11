d2857f79d98f62e702966dd742fb60c184a2ad8b
Joseph O'Doherty <joseph.odoherty@gmail.com>
2016-05-10 21:07:06 -0700

    create artifactnlms3 which builds on artifactfilter

--- gtkclient_tdt/include/artifact_filter.h
+++ gtkclient_tdt/include/artifact_filter.h
@@ -1,13 +1,11 @@
 #ifndef __ARTIFACT_FILTER_H__
 #define	__ARTIFACT_FILTER_H__
 
-#include <string>
+//#include <string>
 #include <armadillo>
 
 using namespace std;
 using namespace arma;
-
-class MatStor;
 
 class ArtifactFilter
 {
@@ -21,9 +19,24 @@ public:
 	~ArtifactFilter();
 
 	mat filter(mat X);
-	void loadWeights(const char *fn);
+	void loadWeights(const char *f);
+	void saveWeights(const char *f);
 	void clearWeights();
 };
+
+class ArtifactNLMS3 : public ArtifactFilter
+{
+protected:
+	double mu;		// learning rate. a small number try 1e-5
+
+public:
+
+	ArtifactNLMS3(int _n);
+	~ArtifactNLMS3();
+	void setMu(float _mu);
+	void train(mat X);
+};
+
 
 #endif
 
--- gtkclient_tdt/src/artifact_filter.cpp
+++ gtkclient_tdt/src/artifact_filter.cpp
@@ -1,3 +1,4 @@
+#include <float.h> // for FLT_EPSILON
 #include "artifact_filter.h"
 
 ArtifactFilter::ArtifactFilter(int _n)
@@ -20,14 +21,19 @@ ArtifactFilter::~ArtifactFilter()
 mat ArtifactFilter::filter(mat X)
 {
 	// Xhat = W * X;
-	return W * X;
+	return W.t() * X;
 }
 
 // load weights from a file
-void ArtifactFilter::loadWeights(const char *fn)
+void ArtifactFilter::loadWeights(const char *f)
 {
-	// automatically detect format type
-	W.load(fn, hdf5_binary_trans);
+	W.load(f, hdf5_binary);
+}
+
+// save weights to a file
+void ArtifactFilter::saveWeights(const char *f)
+{
+	W.save(f, hdf5_binary);
 }
 
 void ArtifactFilter::clearWeights()
@@ -36,3 +42,52 @@ void ArtifactFilter::clearWeights()
 	W.diag().zeros();		// set diag to zero
 }
 
+ArtifactNLMS3::ArtifactNLMS3(int _n) : ArtifactFilter(_n)
+{
+	mu = 1e-5; // reasonable default
+}
+
+ArtifactNLMS3::~ArtifactNLMS3()
+{
+}
+
+void ArtifactNLMS3::setMu(float _mu)
+{
+	mu = (double)_mu;
+}
+
+// X is the input matrix (n by t)
+void ArtifactNLMS3::train(mat X)
+{
+	// in the below we use (roughly) the notation of Haykin, 4th ed. pg 324
+
+	size_t t = X.n_cols;
+
+	// copy X -> Y
+	mat Y(X);
+
+	for (size_t i=0; i<n; i++) {
+
+		X.row(i).zeros();
+
+		for (size_t k=0; k<t; k++) {
+
+			// yhat = w(k) * x(k)
+			double yhat = dot(W.col(i), X.col(k));
+
+			// alpha(k) = y(k) - w'(k) * x(k);
+			double alpha = Y(i, k) - yhat;
+
+			// temp(k) = (mu * alpha(k) (k)) / ||x(k)||^2
+			double xnorm = norm(X.col(k));
+			double temp = (mu * alpha) / ((xnorm * xnorm) + FLT_EPSILON);
+
+			// w(k+1) = w(k) + x(k) * temp(k)
+			// scalar has to come second because of operator overloading
+			W.col(i) += X.col(k) * temp;
+		}
+
+		X.row(i) = Y.row(i);
+	}
+
+}
