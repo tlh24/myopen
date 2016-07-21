8375ae79e4f210369e2067334c10fa5cb934775f
Joseph O'Doherty <joseph.odoherty@gmail.com>
2016-07-21 15:48:13 -0700

    skeleton of some helper programs for autostarting the filters

---
+++ gtkclient_tdt/shutdown.sh
@@ -0,0 +1,8 @@
+
+pkill af
+pkill af2
+pkill bp
+pkill lfp
+pkill notch
+pkill noop
+pkill subtr
---
+++ gtkclient_tdt/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#./po8e
+
+#af ipc:///tmp/broadband.zmq ipc:///tmp/af.zmq > /dev/null &
+#af2 1464840 ipc:///tmp/broadband.zmq ipc:///tmp/af2.zmq > /dev/null &
+
+subtr ipc:///tmp/broadband.zmq ipc:///tmp/events.zmq ipc:///tmp/subtr.zmq > /dev/null &
+
+bp 300 5000 ipc:///tmp/subtr.zmq ipc:///tmp/bp.zmq > /dev/null &
+
+lfp hgamma ipc:///tmp/broadband.zmq ipc:///tmp/lfp.zmq > /dev/null &
+
+notch ipc:///tmp/bp.zmq ipc:///tmp/notch.zmq > /dev/null &
+
+noop ipc:///tmp/lfp.zmq ipc:///tmp/noop.zmq > /dev/null &
