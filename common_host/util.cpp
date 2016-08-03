ab5be80dacefab9a24d548f3972f51be5a566539
Joseph O'Doherty <joseph.odoherty@gmail.com>
2016-08-02 18:28:33 -0700

    make error spit out perror messages when appropriate

--- common_host/util.cpp
+++ common_host/util.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <errno.h>
 #include <cstring>
-//#include <proc/readproc.h>	// for proc_t, openproc, readproc, etc
 #include "util.h"
 
 void warn(const char *s, ...)
@@ -21,8 +21,12 @@ void error(const char *s, ...)
 	va_start(v, s);
 	fprintf(stderr, RED "[ERROR] " RESET BOLD);
 	vfprintf(stderr, s, v);
-	fprintf(stderr, RESET "\n");
+	fprintf(stderr, "\n" RESET);
 	va_end(v);
+	if (errno != 0) {
+		perror(RED "[ERROR] " RESET BOLD);
+	}
+	fprintf(stderr, RESET);
 }
 
 void debug(const char *s, ...)
@@ -36,24 +40,3 @@ void debug(const char *s, ...)
 #endif
 	va_end(v);
 }
-
-// checks if another executable with the same name is running
-// ie are we running already
-/*bool check_running(const char *program_name)
-{
-	pid_t mypid = getpid();
-	PROCTAB *pr = openproc(PROC_FILLSTAT);
-	proc_t pr_info;
-	memset(&pr_info, 0, sizeof(pr_info));
-	bool running = false;
-	while (readproc(pr, &pr_info) != NULL) {
-		if (!strcmp(pr_info.cmd, program_name) &&
-		    pr_info.tgid != mypid) {
-			running = true;
-			break;
-		}
-	}
-	closeproc(pr);
-	return running;
-}
-*/
